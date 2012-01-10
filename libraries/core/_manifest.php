73e8265fb5f2703c2f9afd2d6df6ebde6552ee8c
Tom Wright <tom@inflatablecookie.com>
2012-01-10 17:04:33 +0000

    first commit

---
+++ libraries/core/_manifest.php
@@ -0,0 +1,250 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df;
+
+use df;
+use df\core;
+
+// Exceptions
+interface IException {}
+class LogicException extends \LogicException implements IException {}
+class UnexpectedValueException extends \UnexpectedValueException implements IException {}
+class RuntimeException extends \RuntimeException implements IException {}
+class ApplicationNotFoundException extends RuntimeException {}
+
+
+// Generic interfaces
+interface IStringProvider {
+    public function toString();
+    public function __toString();
+}
+
+
+trait TStringProvider {
+    
+    public function __toString() {
+        try {
+            return (string)$this->toString();
+        } catch(\Exception $e) {
+            return '';
+        }
+    }
+}
+
+
+interface IArrayProvider {
+    public function toArray();
+}
+
+interface IValueMap {
+    public function set($key, $value);
+    public function get($key, $default=null);
+    public function has($key);
+    public function remove($key);
+}
+
+
+interface IValueContainer {
+    public function setValue($value);
+    public function getValue($default=null);
+}
+
+/*
+interface IUserValueContainer extends IValueContainer {
+    public function getStringValue($default='');
+    public function hasValue();
+}
+*/
+
+interface IErrorContainer {
+    public function isValid();
+    public function setErrors(array $errors);
+    public function addErrors(array $errors);
+    public function addError($code, $message);
+    public function getErrors();
+    public function getError($code);
+    public function hasErrors();
+    public function hasError($code);
+    public function clearErrors();
+    public function clearError($code);
+}
+
+trait TErrorContainer {
+    
+    private $_errors = array();
+    
+    public function isValid() {
+        return $this->hasErrors();
+    }
+    
+    public function setErrors(array $errors) {
+        $this->_errors = array();
+        return $this->addErrors($errors);
+    }
+    
+    public function addErrors(array $errors) {
+        foreach($errors as $code => $message) {
+            $this->addError($code, $message);
+        }    
+        
+        return $this;
+    }
+    
+    public function addError($code, $message) {
+        $this->_errors[$code] = $message;
+        return $this;
+    }
+    
+    public function getErrors() {
+        return $this->_errors;
+    }
+    
+    public function getError($code) {
+        if(isset($this->_errors[$code])) {
+            return $this->_errors[$code];
+        }
+        
+        return null;
+    }
+    
+    public function hasErrors() {
+        return !empty($this->_errors);
+    }
+    
+    public function hasError($code) {
+        return isset($this->_errors[$code]);
+    }
+    
+    public function clearErrors() {
+        $this->_errors = array();
+        return $this;
+    }
+    
+    public function clearError($code) {
+        unset($this->_errors[$code]);
+        return $this;
+    }
+}
+
+
+interface IAttributeContainer {
+    public function setAttributes(array $attributes);
+    public function addAttributes(array $attributes);
+    public function getAttributes();
+    public function setAttribute($key, $value);
+    public function getAttribute($key, $default=null);
+    public function removeAttribute($key);
+    public function hasAttribute($key);
+}
+
+
+trait TAttributeContainer {
+    
+    protected $_attributes = array();
+    
+    public function setAttributes(array $attributes) {
+        $this->_attributes = array();
+        return $this->addAttributes($attributes);
+    }
+    
+    public function addAttributes(array $attributes) {
+        foreach($this->_attributes as $key => $value){
+            $this->setAttribute($key, $value);
+        }
+        
+        return $this;
+    }
+    
+    public function getAttributes() {
+        return $this->_attributes;
+    }
+    
+    public function setAttribute($key, $value) {
+        $this->_attributes[$key] = $value;
+        return $this;
+    }
+    
+    public function getAttribute($key, $default=null) {
+        if(isset($this->_attributes[$key])) {
+            return $this->_attributes[$key];
+        }
+        
+        return $default;
+    }
+    
+    public function removeAttribute($key) {
+        unset($this->_attributes[$key]);
+        return $this;
+    }
+    
+    public function hasAttribute($key) {
+        return isset($this->_attributes[$key]);
+    }
+}
+
+
+interface IDumpable {
+    public function getDumpProperties();
+}
+
+
+
+// Loader
+interface ILoader {
+    public function activate();
+    public function deactivate();
+    public function isActive();
+    
+    public function loadClass($class);
+    public function getClassSearchPaths($class);
+    
+    public function findFile($path);
+    public function getFileSearchPaths($path);
+    
+    public function registerLocation($name, $path);
+    public function unregisterLocation($name);
+    public function getLocations();
+}
+
+interface IPackage {}
+
+
+// Applications
+interface IApplication {
+    
+}
+
+interface IRegistryObject {
+    public function getRegistryObjectKey();
+}
+
+interface IApplicationAware {
+    public function getApplication();
+}
+
+trait TApplicationAware {
+    
+    protected $_application;
+    
+    public function getApplication() {
+        return $this->_application;
+    }
+}
+
+
+// Payload
+interface IPayload {}
+interface IDeferredPayload extends IPayload, IApplicationAware {}
+
+
+
+// Config
+interface IConfig extends IApplicationAware, IRegistryObject {
+    public function getDefaultValues();
+    public function getConfigId();
+    public function getConfigValues();
+    public function isConfigDistributed();
+}
