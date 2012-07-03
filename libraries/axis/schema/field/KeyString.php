ec6d2944ca7edb862e6b30447adcf03582a05571
Tom Wright <tom@inflatablecookie.com>
2012-07-03 17:04:28 +0100

    Added keyString and LatLong axis field types
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/axis/schema/field/KeyString.php
@@ -0,0 +1,58 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\axis\schema\field;
+
+use df;
+use df\core;
+use df\axis;
+    
+class KeyString extends String {
+
+	protected $_case = core\string\ICase::NONE;
+
+    protected function _init($length=16, $case=core\string\ICase::NONE) {
+    	$this->setLength($length);
+    	$this->setCase($case);
+    }
+
+    public function setCase($case) {
+    	$case = core\string\Manipulator::normalizeCaseFlag($case);
+
+    	if($case != $this->_case) {
+    		$this->_hasChanged = true;
+    	}
+
+    	$this->_case = $case;
+    	return $this;
+    }
+
+    public function getCase() {
+    	return $this->_case;
+    }
+
+
+// Values
+    public function sanitizeValue($value, $forRecord) {
+    	if($this->_case != core\string\ICase::NONE) {
+    		$value = core\string\Manipulator::applyCase($value, $this->_case, $this->_characterSet);
+    	}
+
+    	return $value;
+    }
+
+// Ext. serialize
+	protected function _importStorageArray(array $data) {
+		parent::_importStorageArray($data);
+		$this->_case = $data['cas'];
+	}
+
+	public function toStorageArray() {
+		$output = parent::toStorageArray();
+		$output['cas'] = $this->_case;
+
+		return $output;
+	}
+}
---
+++ libraries/axis/schema/field/LatLong.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\axis\schema\field;
+
+use df;
+use df\core;
+use df\axis;
+    
+class LatLong extends Base {
+
+    const SCALE = 10;
+    const PRECISION = 6;
+
+
+// Primitive
+	public function toPrimitive(axis\ISchemaBasedStorageUnit $unit, axis\schema\ISchema $schema) {
+		return new opal\schema\Primitive_Decimal($this, self::PRECISION, self::SCALE);
+	}
+
+
+// Ext. serialize
+	protected function _importStorageArray(array $data) {
+		$this->_setBaseStorageArray($data);
+	}
+
+	public function toStorageArray() {
+		return $this->_getBaseStorageArray();
+	}
+}
