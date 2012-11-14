0fa5c2bf3efc3f78eb4c33cf046be5e4a3a76a4d
Tom Wright <tom@inflatablecookie.com>
2012-11-14 15:38:53 +0000

    Fixed translation source in core\validate
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/validate/Handler.php
+++ libraries/core/validate/Handler.php
@@ -120,7 +120,7 @@ class Handler implements IHandler {
     }
     
     public function _($phrase, array $data=null, $plural=null, $locale=null) {
-        $translator = core\i18n\translate\Handler::factory('arch/Context', $locale);
+        $translator = core\i18n\translate\Handler::factory('core/Validate', $locale);
         return $translator->_($phrase, $data, $plural);
     }
 }
