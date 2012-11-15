43bdaed945b8a9704f893dae234e1975fa3d1937
Tom Wright <tom@inflatablecookie.com>
2012-11-15 15:56:22 +0000

    Added stub for html editor widget
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/aura/html/widget/HtmlEditor.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\aura\html\widget;
+
+use df;
+use df\core;
+use df\aura;
+use df\arch;
+    
+class HtmlEditor extends Textarea {
+
+    
+}
