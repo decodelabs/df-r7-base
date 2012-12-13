993b586a3eb4bfd8e86e29757805296c01e99f1a
Tom Wright <tom@inflatablecookie.com>
2012-12-13 00:11:15 +0000

    Added task response library
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/halo/task/Response.php
@@ -0,0 +1,110 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\halo\task;
+
+use df;
+use df\core;
+use df\halo;
+    
+class Response implements IResponse {
+
+    protected $_channels = array();
+
+    public function __construct(array $channels=null) {
+        if($channels !== null) {
+            $this->addChannels($channels);
+        }
+    }
+
+    public function setChannels(array $channels) {
+        $this->_channels = array();
+        return $this->addChannels($channels);
+    }
+
+    public function addChannels(array $channels) {
+        foreach($channels as $channel) {
+            if($channel instanceof core\io\IChannel) {
+                $this->addChannel($channel);
+            }
+        }
+
+        return $this;
+    }
+
+    public function addChannel(core\io\IChannel $channel) {
+        $this->_channels[$channel->getChannelId()] = $channel;
+        return $this;
+    }
+
+    public function hasChannel($id) {
+        if($id instanceof core\io\IChannel) {
+            $id = $id->getChannelId();
+        }
+
+        return isset($this->_channels[$id]);
+    }
+
+    public function getChannel($id) {
+        if($id instanceof core\io\IChannel) {
+            $id = $id->getChannelId();
+        }
+
+        if(isset($this->_channels[$id])) {
+            return $this->_channels[$id];
+        }
+    }
+
+    public function removeChannel($id) {
+        if($id instanceof core\io\IChannel) {
+            $id = $id->getChannelId();
+        }
+
+        unset($this->_channels[$id]);
+        return $this;
+    }
+
+    public function getChannels() {
+        return $this->_channels;
+    }
+
+    public function clearChannels() {
+        $this->_channels = array();
+        return $this;
+    }
+
+
+    public function write($data) {
+        foreach($this->_channels as $channel) {
+            $channel->write($data);
+        }
+
+        return $this;
+    }
+
+    public function writeLine($line) {
+        foreach($this->_channels as $channel) {
+            $channel->writeLine($line);
+        }
+
+        return $this;
+    }
+
+    public function writeError($error) {
+        foreach($this->_channels as $channel) {
+            $channel->writeError($error);
+        }
+
+        return $this;
+    }
+
+    public function writeErrorLine($line) {
+        foreach($this->_channels as $channel) {
+            $channel->writeErrorLine($line);
+        }
+
+        return $this;
+    }
+}
---
+++ libraries/halo/task/_manifest.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\halo\task;
+
+use df;
+use df\core;
+use df\halo;
+use df\arch;
+
+    
+// Exceptions
+interface IException {}
+
+
+
+// Interfaces
+interface IResponse {
+    public function setChannels(array $channels);
+    public function addChannels(array $channels);
+    public function addChannel(core\io\IChannel $channel);
+    public function hasChannel($id);
+    public function getChannel($id);
+    public function removeChannel($id);
+    public function getChannels();
+    public function clearChannels();
+
+    public function write($data);
+    public function writeLine($line);
+    public function writeError($error);
+    public function writeErrorLine($line);
+}
+
