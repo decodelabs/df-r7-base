638d60c1102287b77ba290cdf9bff96d904a597b
Tom Wright <tom@inflatablecookie.com>
2012-12-13 00:18:25 +0000

    Added gitignore init task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/_actions/TaskInitGitignore.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\halo;
+use df\arch;
+    
+class TaskInitGitignore extends arch\Action {
+
+    public function execute() {
+        $response = new halo\task\Response([
+            new core\io\channel\Std()
+        ]);
+
+        $response->writeLine('Copying default .gitignore file...');
+
+        $path = df\Launchpad::$applicationPath;
+
+        core\io\Util::copyFile(__DIR__.'/default.gitignore', $path.'/.gitignore');
+        core\io\Util::chmod($path.'/.gitignore', 0777);
+
+        if(is_file($path.'/.gitignore')) {
+            $response->writeLine('Done');
+        } else {
+            $response->writeError('Unable to copy .gitignore file');
+        }
+    }
+}
