3f97d03fda7bb2a859b81386b2dc3ce1da6d377d
Tom Wright <tom@inflatablecookie.com>
2013-02-05 17:45:41 +0000

    Added SlugTreeBreadcrumbs component
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/shared/_components/SlugTreeBreadcrumbs.php
@@ -0,0 +1,63 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\shared\_components;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\axis;
+    
+class SlugTreeBreadcrumbs extends arch\Component {
+
+    protected $_node;
+
+    protected function _init($node=null) {
+        if($node) {
+            $this->setNode($node);
+        }
+    }
+
+    public function setNode(axis\unit\table\record\SlugTreeRecord $node) {
+        $this->_node = $node;
+        return $this;
+    }
+
+    public function getNode() {
+        return $this->_node;
+    }
+
+    protected function _execute() {
+        $trail = $this->_node->fetchParentPath();
+        $trail[] = $this->_node;
+
+        $output = new arch\navigation\breadcrumbs\EntryList();
+
+        foreach($trail as $node) {
+            $request = clone $this->request;
+            $slug = $node['slug'];
+
+            if(empty($slug)) {
+                unset($request->query->slug);
+            } else {
+                $request->query->slug = $slug;
+            }
+
+            $output->addEntry(
+                $output->newLink($request, $node['name'])  
+                    ->setId($slug)
+                    ->setDescription($node['description'])
+                    ->setIcon('folder')
+            );
+        }
+        
+        $view = $this->getRenderTarget()->getView();
+
+        return $view->html
+            ->breadcrumbList($output)
+                ->setSeparator('/');
+    }
+}
