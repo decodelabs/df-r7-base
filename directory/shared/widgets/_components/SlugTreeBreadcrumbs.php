7d052352b5ffb451924809f021106a11042f3657
Tom Wright <tom@inflatablecookie.com>
2013-02-06 13:27:13 +0000

    Moved SlugTreeBreadcrumbs component
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/shared/widgets/_components/SlugTreeBreadcrumbs.php
+++ directory/shared/widgets/_components/SlugTreeBreadcrumbs.php
@@ -3,7 +3,7 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\apex\directory\shared\_components;
+namespace df\apex\directory\shared\widgets\_components;
 
 use df;
 use df\core;
