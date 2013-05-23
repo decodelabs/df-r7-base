a8855691098836a485012c6723614f1ff887b246
Tom Wright <tom@inflatablecookie.com>
2013-05-23 20:48:09 +0100

    Added ifNotExists to InsertRecord task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/opal/record/task/InsertRecord.php
+++ libraries/opal/record/task/InsertRecord.php
@@ -14,6 +14,8 @@ class InsertRecord implements IInsertRecordTask {
     use TTask;
     use TRecordTask;
 
+    protected $_ifNotExists = false;
+
     public function __construct(opal\record\IRecord $record) {
         $this->_record = $record;
         $this->_setId(opal\record\Base::extractRecordId($record));
@@ -22,11 +24,23 @@ class InsertRecord implements IInsertRecordTask {
     public function getRecordTaskName() {
         return 'Insert';
     }
+
+    public function ifNotExists($flag=null) {
+        if($flag !== null) {
+            $this->_ifNotExists = (bool)$flag;
+            return $this;
+        }
+
+        return $this->_ifNotExists;
+    }
     
     public function execute(opal\query\ITransaction $transaction) {
         $data = $this->_record->getValuesForStorage();
         
-        $query = $transaction->insert($data)->into($this->getAdapter());
+        $query = $transaction->insert($data)
+            ->into($this->getAdapter())
+            ->ifNotExists((bool)$this->_ifNotExists);
+            
         $id = $query->execute();
         $row = $query->getRow();
         
