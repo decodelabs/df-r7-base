033dea223a91d21fe69f5cc53ccd6a08021859e4
Tom Wright <tom@inflatablecookie.com>
2013-06-24 13:20:07 +0100

    Added progressBar widget
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/aura/html/widget/ProgressBar.php
@@ -0,0 +1,64 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\aura\html\widget;
+
+use df;
+use df\core;
+use df\aura;
+use df\arch;
+   
+class ProgressBar extends Base {
+
+    const PRIMARY_TAG = 'progress';
+
+    protected $_value;
+    protected $_min;
+    protected $_max;
+
+    public function __construct(arch\IContext $context, $value, $max=100, $min=0) {
+        $this->setValue($value);
+        $this->setMax($max);
+        $this->setMin($min);
+    }
+
+    protected function _render() {
+        $view = $this->getRenderTarget();
+        $tag = $this->getTag();
+
+        $tag->setAttribute('value', $this->_value);
+        $tag->setAttribute('min', $this->_min);
+        $tag->setAttribute('max', $this->_max);
+
+        return $tag->render();
+    }
+
+    public function setValue($value) {
+        $this->_value = $value;
+        return $this;
+    }
+
+    public function getValue() {
+        return $this->_value;
+    }
+
+    public function setMin($min) {
+        $this->_min = $min;
+        return $this;
+    }
+
+    public function getMin() {
+        return $this->_min;
+    }
+
+    public function setMax($max) {
+        $this->_max = $max;
+        return $this;
+    }
+
+    public function getMax() {
+        return $this->_max;
+    }
+}
