8b869d028b16801f3695a68c68735b10de9e5f45
Tom Wright <tom@inflatablecookie.com>
2013-06-24 21:44:45 +0100

    Refactored git tasks
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/git/_actions/TaskUpdate.php
+++ directory/front/git/_actions/TaskUpdate.php
@@ -3,7 +3,7 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\apex\directory\front\packages\_actions;
+namespace df\apex\directory\front\git\_actions;
 
 use df;
 use df\core;
@@ -11,19 +11,19 @@ use df\apex;
 use df\halo;
 use df\arch;
     
-class TaskRefresh extends arch\task\Action {
+class TaskUpdate extends arch\task\Action {
 
     protected function _run() {
         $name = $this->request->query['package'];
 
         if(empty($name)) {
-            return $this->directory->newRequest('packages/refresh-all');
+            return $this->directory->newRequest('git/update-all');
         }
 
-        $this->response->writeLine('Refreshing package "'.$name.'"');
+        $this->response->writeLine('Pulling updates for package "'.$name.'"');
         $model = $this->data->getModel('package');
 
-        if(!$result = $model->updateRemote($name)) {
+        if(!$result = $model->pull($name)) {
             $this->response->writeLine('!! Package "'.$name.'" repo could not be found !!');
         } else {
             $this->response->write($result."\n");
