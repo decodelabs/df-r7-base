95abfdf2fe244bbc779aadff813284d70b1da36b
Tom Wright <tom@inflatablecookie.com>
2013-07-30 23:44:29 +0100

    Added templated notifications
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/plug/shared/Comms.php
+++ libraries/plug/shared/Comms.php
@@ -9,6 +9,8 @@ use df;
 use df\core;
 use df\plug;
 use df\flow;
+use df\arch;
+use df\aura;
     
 class Comms implements core\ISharedHelper {
 
@@ -51,8 +53,31 @@ class Comms implements core\ISharedHelper {
         return $this->sendNotification($this->newNotification($subject, $body, $to));
     }
 
-    public function newNotification($subject, $body, $to=null) {
-        return $this->_manager->newNotification($subject, $body, $to);
+    public function templateNotify($path, $contextRequest, array $args=array(), $to=null, $from=null) {
+        return $this->sendNotification($this->newTemplateNotification($path, $contextRequest, $args, $to, $from));
+    }
+
+    public function newNotification($subject, $body, $to=null, $from=null) {
+        return $this->_manager->newNotification($subject, $body, $to, $from);
+    }
+
+    public function newTemplateNotification($path, $contextRequest, array $args=array(), $to=null, $from=null) {
+        if($this->_context instanceof arch\IContext) {
+            $aura = $this->_context->aura;
+        } else {
+            $aura = arch\Context::factory($this->_context->getApplication())->aura;
+        }
+
+        $view = $aura->getView($path, $contextRequest);
+
+        if(!$view instanceof aura\view\Notification) {
+            throw new aura\view\InvalidArgumentException(
+                'Templated notifications can only use .notification templates'
+            );
+        }
+
+        $view->setArgs($args);
+        return $view->toNotification($to, $from);
     }
 
     public function sendNotification(flow\INotification $notification) {
