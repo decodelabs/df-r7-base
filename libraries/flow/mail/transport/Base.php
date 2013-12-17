f81d79cb9ff35302df7515f31e242a3b5fd78cc2
Tom Wright <tom@inflatablecookie.com>
2013-12-17 12:50:29 +0000

    Fixed mail transport settings handling
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/flow/mail/transport/Base.php
+++ libraries/flow/mail/transport/Base.php
@@ -48,10 +48,11 @@ abstract class Base implements flow\mail\ITransport {
 
         if($name === null) {
             $name = self::getDefaultTransportName();
-            $class = self::getTransportClass($name);
-            $config = flow\mail\Config::getInstance();
-            $settings = $config->getDefaultTransportSettings($name);
         }
+
+        $class = self::getTransportClass($name);
+        $config = flow\mail\Config::getInstance();
+        $settings = $config->getTransportSettings($name);
 
         return new $class($settings);
     }
