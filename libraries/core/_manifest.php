12daf36510618ff812d0f3d8369778be79507bfd
Tom Wright <tom@inflatablecookie.com>
2014-04-13 13:43:37 +0100

    Added translation proxy to TContext
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/_manifest.php
+++ libraries/core/_manifest.php
@@ -760,6 +760,15 @@ trait TContext {
                 return $this->getHelper($key);
         }
     }
+
+    public function _($phrase, array $data=null, $plural=null, $locale=null) {
+        if($locale === null) {
+            $locale = $this->_locale;
+        }
+
+        $translator = core\i18n\translate\Handler::factory('core/Context', $locale);
+        return $translator->_($phrase, $data, $plural);
+    }
 }
 
 class SharedContext implements IContext {
