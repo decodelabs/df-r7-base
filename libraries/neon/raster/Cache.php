903b08aa649bb7614663cf7997d9fa58cb884279
Tom Wright <tom@inflatablecookie.com>
2014-05-19 12:59:29 +0100

    Added remote file support to neon raster cache handler
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/neon/raster/Cache.php
+++ libraries/neon/raster/Cache.php
@@ -8,23 +8,56 @@ namespace df\neon\raster;
 use df;
 use df\core;
 use df\neon;
+use df\link;
     
 class Cache extends core\cache\Base {
 
     const USE_DIRECT_FILE_BACKEND = true;
     const IS_DISTRIBUTED = false;
-    const DEFAULT_LIFETIME = 86400;
+    const DEFAULT_LIFETIME = 86400; // 1 day
+    const URL_LIFETIME = 31536000; // 1 year
 
-    public function getTransformationFilePath($sourceFilePath, $transformation) {
-        $keyPath = core\io\Util::stripLocationFromFilePath($sourceFilePath);
-        $key = basename(dirname($keyPath)).'_'.basename($keyPath).'-'.md5($keyPath.':'.$transformation);
-        $mTime = filemtime($sourceFilePath);
+    public function getTransformationFilePath($sourceFilePath, $transformation, core\time\IDate $modificationDate=null) {
+        $mTime = null;
+        $isUrl = false;
+        $lifetime = static::DEFAULT_LIFETIME;
 
-        if($mTime > $this->getCreationTime($key)) {
+        if($sourceFilePath instanceof link\http\IUrl) {
+            $isUrl = true;
+            $path = (string)$sourceFilePath->getPath();
+            $key = basename(dirname($path)).'_'.basename($path).'-'.md5($sourceFilePath.':'.$transformation);
+
+            if($modificationDate !== null) {
+                $mTime = $modificationDate->toTimestamp();
+            }
+
+            $lifetime = static::URL_LIFETIME;
+        } else {
+            $keyPath = core\io\Util::stripLocationFromFilePath($sourceFilePath);
+            $key = basename(dirname($keyPath)).'_'.basename($keyPath).'-'.md5($keyPath.':'.$transformation);
+            $mTime = filemtime($sourceFilePath);
+        }
+        
+
+        if($mTime !== null && $mTime > $this->getCreationTime($key)) {
             $this->remove($key);
         }
 
         if(!$output = $this->getDirectFilePath($key)) {
+            if($isUrl) {
+                $http = new link\http\Client();
+                $file = core\io\channel\File::createTempFile();
+                $response = $http->getFile($sourceFilePath, $file);
+
+                if(!$response->isOk()) {
+                    throw new RuntimeException(
+                        'Unable to fetch remote image for transformation'
+                    );
+                }
+
+                $sourceFilePath = $file->getPath();
+            }
+
             try {
                 $image = Image::loadFile($sourceFilePath)->setOutputFormat('PNG32');
             } catch(FormatException $e) {
@@ -33,8 +66,16 @@ class Cache extends core\cache\Base {
 
             $image->transform($transformation)->apply();
 
-            $this->set($key, $image->toString());
+            $this->set($key, $image->toString(), $lifetime);
             $output = $this->getDirectFilePath($key);
+
+            if($isUrl) {
+                $file->close();
+
+                try {
+                    core\io\Util::deleteFile($file->getPath());
+                } catch(\Exception $e) {}
+            }
         }
 
         return $output;
