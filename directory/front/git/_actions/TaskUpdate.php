c62c46b35ae96d7d9777c1cd4160e6426651d6a6
Tom Wright <tom@inflatablecookie.com>
2014-05-24 12:05:44 +0100

    Call application/build-custom when pulling app repo
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/git/_actions/TaskUpdate.php
+++ directory/front/git/_actions/TaskUpdate.php
@@ -36,7 +36,9 @@ class TaskUpdate extends arch\task\Action {
         }
 
         if(is_dir($this->application->getLocalDataStoragePath().'/run')) {
-            return $this->directory->newRequest('application/build?testing=1');
+            $this->runChild('application/build?testing=1');
+        } else if($this->directory->actionExists('application/build-custom') && in_array('app', $names)) {
+            $this->runChild('application/build-custom');
         }
     }
 }
