29c9940766439d9f5c717b69b760e0c9047b6efb
Tom Wright <tom@inflatablecookie.com>
2014-08-08 13:55:08 +0100

    Added SASS bridge
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/aura/css/sass/Bridge.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\aura\css\sass;
+
+use df;
+use df\core;
+use df\aura;
+use df\arch;
+use df\link;
+use df\halo;
+
+class Bridge implements IBridge {
+    
+    protected $_fileName;
+    protected $_type;
+    protected $_sourceDir;
+    protected $_workDir;
+    protected $_key;
+    protected $_isDevelopment;
+    protected $_context;
+
+    public function __construct(arch\IContext $context, $path) {
+        $this->_context = $context;
+        $path = realpath($path);
+
+        if(!is_file($path)) {
+            throw new RuntimeException(
+                'Sass file not found'
+            );
+        }
+
+        $this->_sourceDir = dirname($path);
+        $basename = basename($path);
+
+        $this->_fileName = substr($basename, 0, -5);
+        $this->_type = strtolower(substr($basename, -4));
+
+        $this->_workDir = $context->application->getLocalStoragePath().'/sass';
+
+        $this->_isDevelopment = $this->_context->application->isDevelopment();
+        $this->_key = md5($path);
+    }
+
+
+    public function getHttpResponse() {
+        $path = $this->getCompiledPath();
+
+        $output = $this->_context->http->fileResponse($path);
+        $output->setContentType('text/css');
+        $headers = $output->getHeaders();
+
+        if(!$this->_isDevelopment) {
+            $headers->setCacheAccess('no-cache')
+                ->canStoreCache(false)
+                ->shouldRevalidateCache(true);
+        } else {
+            $headers->setCacheExpiration('+1 year');
+        }
+
+        return $output;
+    }
+
+    public function getCompiledPath() {
+        $filePath = $this->_workDir.'/'.$this->_key.'.css';
+
+        if(!is_file($filePath)) {
+            return $this->compile();
+        }
+
+        $mtime = filemtime($filePath);
+
+        if($this->_isDevelopment) {
+            $manifestPath = $this->_workDir.'/'.$this->_key.'.json';
+
+            if(!is_file($manifestPath)) {
+                $this->compile();
+            } else {
+                $files = json_decode(file_get_contents($manifestPath), true);
+
+                foreach($files as $file) {
+                    if($mtime < filemtime($file)) {
+                        $this->compile();
+                        break;
+                    }
+                }
+            }
+        } else {
+            if($mtime < df\Launchpad::COMPILE_TIMESTAMP) {
+                $this->compile();
+            }
+        }
+
+        return $filePath;
+    }
+
+    public function compile() {
+        core\io\Util::ensureDirExists($this->_workDir.'/'.$this->_key);
+        $sourceFiles = $this->findSourceFiles();
+        $manifest = [];
+
+        while(!empty($sourceFiles)) {
+            $filePath = array_shift($sourceFiles);
+            $fileKey = md5($filePath);
+            $fileType = substr($filePath, -4);
+            $manifest[$fileKey] = $filePath;
+
+            $contents = file_get_contents($filePath);
+            preg_match_all('/\@import \"([^"]+)\"\;/', $contents, $matches);
+
+            if(!empty($matches[1])) {
+                $imports = [];
+
+                foreach($matches[1] as $path) {
+                    $char = $path{0};
+
+                    if($char == '/') {
+                        $importPath = $path;
+                    } else if($char == '#') {
+                        $importPath = df\Launchpad::$loader->findFile(substr($path, 1));
+                    } else {
+                        $importPath = realpath(dirname($filePath).'/'.$path);
+                    }
+
+                    $key = md5($importPath);
+                    $type = substr($importPath, -4);
+                    $imports[$path] = $key.'.'.$type;
+
+                    if(!isset($manifest[$key])) {
+                        $sourceFiles[] = $importPath;
+                    }
+                }
+
+                foreach($imports as $import => $keyName) {
+                    $contents = str_replace('@import "'.$import.'"', '@import "'.$keyName.'"', $contents);
+                }
+            }
+
+            file_put_contents($this->_workDir.'/'.$this->_key.'/'.$fileKey.'.'.$fileType, $contents);
+        }
+
+        file_put_contents($this->_workDir.'/'.$this->_key.'.json', json_encode(array_values($manifest)));
+
+        $result = halo\process\launcher\Base::factory('sass', [
+                $this->_workDir.'/'.$this->_key.'/'.$this->_key.'.'.$this->_type, 
+                $this->_workDir.'/'.$this->_key.'.css'
+            ])
+            ->setWorkingDirectory($this->_workDir)
+            ->launch();
+
+        if($result->hasError()) {
+            throw new RuntimeException(
+                $result->getError()
+            );
+        }
+
+        core\io\Util::deleteDir($this->_workDir.'/'.$this->_key);
+
+        return $this->_workDir.'/'.$this->_key.'.css';
+    }
+
+    public function findSourceFiles() {
+        $stripped = core\io\Util::stripLocationFromFilePath($this->_sourceDir);
+        $parts = explode('://', $stripped, 2);
+
+        if(count($parts) == 1) {
+            return [$this->_fileName.'.'.$this->_type => $this->_sourceDir.'/'.$this->_fileName.'.'.$this->_type];
+        }
+
+        $parts = explode('/', array_pop($parts));
+
+        switch($parts[0]) {
+            case 'libraries':
+                array_shift($parts);
+                break;
+
+            case 'themes':
+            case 'directory':
+            case 'models':
+            case 'hooks':
+            case 'daemons':
+                array_unshift($parts, 'apex');
+                break;
+
+            default:
+                return [$this->_fileName.'.'.$this->_type => $this->_sourceDir.'/'.$this->_fileName.'.'.$this->_type];
+        }
+
+        $path = implode('/', $parts);
+        return df\Launchpad::$loader->lookupFileListRecursive($path, [$this->_type]);
+    }
+}
---
+++ libraries/aura/css/sass/_manifest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\aura\css\sass;
+
+use df;
+use df\core;
+use df\aura;
+use df\arch;
+use df\link;
+
+// Exceptions
+interface IException {}
+class RuntimeException extends \RuntimeException implements IException {}
+
+
+// Interfaces
+interface IBridge {
+    public function getHttpResponse();
+    public function getCompiledPath();
+    public function compile();
+    public function findSourceFiles();
+}
