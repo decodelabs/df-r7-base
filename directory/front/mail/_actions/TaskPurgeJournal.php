37eda186baf2a56d9d3c453ac3c7cb1716a535c2
Tom Wright <tom@inflatablecookie.com>
2014-09-29 20:49:45 +0100

    Added mail journal purge task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/mail/_actions/TaskPurgeJournal.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\mail\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+
+class TaskPurgeJournal extends arch\task\Action {
+    
+    const SCHEDULE = '0 3 * * *';
+    const SCHEDULE_AUTOMATIC = true;
+
+    public function execute() {
+        $deleted = $this->data->mail->journal->delete()
+            ->where('expireDate', '!=', null)
+            ->where('expireDate', '<', 'now')
+            ->execute();
+
+        $this->response->writeLine('Purged '.$deleted.' mail journal records');
+    }
+}
