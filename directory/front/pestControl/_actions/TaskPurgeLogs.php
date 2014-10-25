46e4ebf2283a8232af137bf74204eb67893c99d7
Tom Wright <tom@inflatablecookie.com>
2014-10-25 16:48:20 +0100

    Added pest control purge task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/pestControl/_actions/TaskPurgeLogs.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\pestControl\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+
+class TaskPurgeLogs extends arch\task\Action {
+    
+    const SCHEDULE = '0 16 * * *';
+    const SCHEDULE_AUTOMATIC = true;
+
+    public function execute() {
+        $errors = $this->data->pestControl->errorLog->delete()
+            ->where('isArchived', '=', false)
+            ->where('date', '<', '-3 months')
+            ->execute();
+
+        $this->io->writeLine('Purged '.$errors.' critical error logs');
+
+        $misses = $this->data->pestControl->missLog->delete()
+            ->where('isArchived', '=', false)
+            ->where('date', '<', '-3 months')
+            ->execute();
+
+        $this->io->writeLine('Purged '.$misses.' miss logs');
+
+        $accesses = $this->data->pestControl->accessLog->delete()
+            ->where('isArchived', '=', false)
+            ->where('date', '<', '-3 months')
+            ->execute();
+
+        $this->io->writeLine('Purged '.$accesses.' access logs');
+    }
+}
