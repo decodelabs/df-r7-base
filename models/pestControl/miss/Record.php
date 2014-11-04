cc5114dcc1ffce6832f706c232acf25a4d320c8b
Tom Wright <tom@inflatablecookie.com>
2014-11-04 15:06:16 +0000

    Added delete cascade to miss logs
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ models/pestControl/miss/Record.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\models\pestControl\miss;
+
+use df;
+use df\core;
+use df\apex;
+use df\opal;
+use df\axis;
+
+class Record extends opal\record\Base {
+    
+    protected function _onPreDelete($taskSet, $task) {
+        $id = $this['id'];
+
+        $deleteTask = $taskSet->addRawQuery(
+            'deleteLogs:'.$id,
+            $this->getRecordAdapter()->getModel()->missLog->delete()
+                ->where('miss', '=', $id)
+        );
+
+        $task->addDependency($deleteTask);
+    }
+}
