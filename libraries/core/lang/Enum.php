1e15c9b4a5a8b33e9e97aaf848d5574c3e16aecf
Tom Wright <tom@inflatablecookie.com>
2014-11-14 15:15:38 +0000

    Fixed enum comparison
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/lang/Enum.php
+++ libraries/core/lang/Enum.php
@@ -97,7 +97,7 @@ abstract class Enum implements IEnum, core\IDumpable {
     }
 
     public function is($value) {
-        return $this->_index == self::factory($value);
+        return $this->_index == self::factory($value)->_index;
     }
 
     public static function __callStatic($name, array $args) {
