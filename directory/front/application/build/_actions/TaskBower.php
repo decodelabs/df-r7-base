ef70598085f5b94806e5caafde46ccda6197e78c
Tom Wright <tom@inflatablecookie.com>
2014-11-29 14:56:15 +0000

    Added auto bower task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/application/build/_actions/TaskBower.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\apex\directory\front\application\build\_actions;
+
+use df;
+use df\core;
+use df\apex;
+use df\arch;
+use df\halo;
+    
+class TaskBower extends arch\task\Action {
+
+    public function execute() {
+        if(!is_file($this->application->getApplicationPath().'/bower.json')) {
+            return;
+        }
+
+        $this->io->writeLine('Calling bower');
+
+        halo\process\launcher\Base::factory('bower install')
+            ->setWorkingDirectory($this->application->getApplicationPath())
+            ->setMultiplexer($this->io)
+            ->launch();
+    }
+}
