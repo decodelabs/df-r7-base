3c287c969cf2409e82e5632b9fce70182cad123f
Tom Wright <tom@inflatablecookie.com>
2014-12-03 17:06:50 +0000

    Updated touch icon facet
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/aura/theme/facet/TouchIcons.php
+++ libraries/aura/theme/facet/TouchIcons.php
@@ -16,21 +16,21 @@ class TouchIcons extends Base {
             // Chrome android
             ->addLink('touch-chrome-android', 'icon', 'touch-icon-192x192.png?cts', ['sizes' => '192x192'])
             // iPhone 6 plus (3x)
-            ->addLink('touch-apple-180', 'apple-touch-icon-precomposed', 'apple-touch-icon-180x180-precomposed.png?cts', ['sizes' => '180x180'])
+            ->addLink('touch-apple-180', 'apple-touch-icon', 'apple-touch-icon-180x180.png?cts', ['sizes' => '180x180'])
             // iPad IOS 7+ (2x)
-            ->addLink('touch-apple-152', 'apple-touch-icon-precomposed', 'apple-touch-icon-152x152-precomposed.png?cts', ['sizes' => '152x152'])
+            ->addLink('touch-apple-152', 'apple-touch-icon', 'apple-touch-icon-152x152.png?cts', ['sizes' => '152x152'])
             // iPad IOS <6 (2x)
-            ->addLink('touch-apple-144', 'apple-touch-icon-precomposed', 'apple-touch-icon-144x144-precomposed.png?cts', ['sizes' => '144x144'])
+            ->addLink('touch-apple-144', 'apple-touch-icon', 'apple-touch-icon-144x144.png?cts', ['sizes' => '144x144'])
             // iPhone IOS 7+ (2x)
-            ->addLink('touch-apple-120', 'apple-touch-icon-precomposed', 'apple-touch-icon-120x120-precomposed.png?cts', ['sizes' => '120x120'])
+            ->addLink('touch-apple-120', 'apple-touch-icon', 'apple-touch-icon-120x120.png?cts', ['sizes' => '120x120'])
             // iPhone IOS <6 (2x)
-            ->addLink('touch-apple-114', 'apple-touch-icon-precomposed', 'apple-touch-icon-114x114-precomposed.png?cts', ['sizes' => '114x114'])
+            ->addLink('touch-apple-114', 'apple-touch-icon', 'apple-touch-icon-114x114.png?cts', ['sizes' => '114x114'])
             // iPad mini IOS 7+ (1x)
-            ->addLink('touch-apple-76', 'apple-touch-icon-precomposed', 'apple-touch-icon-76x76-precomposed.png?cts', ['sizes' => '76x76'])
+            ->addLink('touch-apple-76', 'apple-touch-icon', 'apple-touch-icon-76x76.png?cts', ['sizes' => '76x76'])
             // iPad mini IOS <6 (1x)
-            ->addLink('touch-apple-72', 'apple-touch-icon-precomposed', 'apple-touch-icon-72x72-precomposed.png?cts', ['sizes' => '72x72'])
+            ->addLink('touch-apple-72', 'apple-touch-icon', 'apple-touch-icon-72x72.png?cts', ['sizes' => '72x72'])
             // Non retina apple, android 2.1+
-            ->addLink('touch-sd', 'apple-touch-icon-precomposed', 'apple-touch-icon-57x57-precomposed.png?cts')
+            ->addLink('touch-sd', 'apple-touch-icon', 'apple-touch-icon-57x57.png?cts')
             // IE
             ->setMeta('msapplication-TileImage', $view->uri('touch-icon-150x150.png?cts'));
     }
