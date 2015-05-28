0ddc3ddd16dadee7672558c33b821d2aa66dfcd0
Tom Wright <tom@inflatablecookie.com>
2015-05-28 16:30:21 +0100

    Updated default gitignore
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/config/_actions/TaskInit.php
+++ directory/front/config/_actions/TaskInit.php
@@ -13,7 +13,32 @@ use df\axis;
 
 class TaskInit extends arch\task\Action {
     
+    public function extractCliArguments(core\cli\ICommand $command) {
+        foreach($command->getArguments() as $arg) {
+            if(!$arg->isOption()) {
+                $this->request->query->environments[] = (string)$arg;
+            }
+        }
+    }
+
     public function execute() {
+        if(!empty($this->request->query->environments)) {
+            foreach($this->request->query->environments as $envNode) {
+                core\Config::clearLiveCache();
+                $currentEnv = df\Launchpad::$environmentId;
+                df\Launchpad::$environmentId = $envNode->getValue();
+
+                $this->_apply();
+            }
+
+            df\Launchpad::$environmentId = $currentEnv;
+            core\Config::clearLiveCache();
+        } else {
+            $this->_apply();
+        }
+    }
+
+    protected function _apply() {
         $this->io->write('Looking up config classes in:');
         $libList = df\Launchpad::$loader->lookupLibraryList();
         $classes = [];
