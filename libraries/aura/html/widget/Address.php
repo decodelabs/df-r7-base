364777c5e4c90bcc7cc1bc1ee153d16aa0404a6b
Tom Wright <tom@inflatablecookie.com>
2015-11-20 11:07:59 +0000

    Added country switch flag to address widget
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/aura/html/widget/Address.php
+++ libraries/aura/html/widget/Address.php
@@ -12,20 +12,21 @@ use df\user;
 use df\arch;
 
 class Address extends Base implements core\IDumpable {
-    
+
     const PRIMARY_TAG = 'div';
-    
+
     const SHORT = 'short';
     const LONG = 'long';
     const FULL = 'full';
-    
+
     protected $_address;
     protected $_mode = self::FULL;
-    
+    protected $_shouldShowCountry = true;
+
     public function __construct(arch\IContext $context, $address=null) {
         $this->setAddress($address);
     }
-    
+
     public function setAddress($address=null) {
         if(is_array($address)) {
             $address = user\PostalAddress::fromArray($address);
@@ -36,11 +37,20 @@ class Address extends Base implements core\IDumpable {
         $this->_address = $address;
         return $this;
     }
-    
+
     public function getAddress() {
         return $this->_address;
     }
-    
+
+    public function shouldShowCountry($flag=null) {
+        if($flag !== null) {
+            $this->_shouldShowCountry = (bool)$flag;
+            return $this;
+        }
+
+        return $this->_shouldShowCountry;
+    }
+
     protected function _render() {
         if($this->_address === null) {
             return null;
@@ -49,7 +59,7 @@ class Address extends Base implements core\IDumpable {
         $tag = $this->getTag();
         $content = new aura\html\ElementContent();
         $view = $this->getRenderTarget()->getView();
-        
+
         $poBox = $this->_address->getPostOfficeBox();
         $streetAddress = $this->_address->getMainStreetLine();
         $extendedAddress = $this->_address->getExtendedStreetLine();
@@ -57,54 +67,54 @@ class Address extends Base implements core\IDumpable {
         $region = $this->_address->getRegion();
         $postcode = $this->_address->getPostalCode();
         $countryCode = $this->_address->getCountryCode();
-        
+
         $isFull = $this->_mode == self::FULL;
         $isShort = $this->_mode == self::SHORT;
-        
+
         if($isFull) {
             $blockTag = 'div';
         } else {
             $blockTag = 'span';
         }
-        
+
         $tag->setName($blockTag);
-        
+
         if(!empty($poBox)) {
             $content->push(new aura\html\Element($blockTag, $poBox, ['class' => 'post-office-box']));
         }
-        
+
         if(!empty($streetAddress)) {
             if(!$isFull && !$content->isEmpty()) {
                 $content->push(', ');
             }
-            
+
             $content->push(new aura\html\Element($blockTag, $streetAddress, ['class' => 'street-address']));
         }
-        
+
         if(!empty($extendedAddress)) {
             if(!$isFull && !$content->isEmpty()) {
                 $content->push(', ');
             }
-            
+
             $content->push(new aura\html\Element($blockTag, $extendedAddress, ['class' => 'extended-address']));
         }
-        
+
         if(!$isShort && !empty($locality)) {
             if(!$isFull && !$content->isEmpty()) {
                 $content->push(', ');
             }
-            
+
             $content->push(new aura\html\Element($blockTag, $locality, ['class' => 'locality']));
         }
-        
+
         if(!$isShort && !empty($region)) {
             if(!$isFull && !$content->isEmpty()) {
                 $content->push(', ');
             }
-            
+
             $content->push(new aura\html\Element($blockTag, $region, ['class' => 'region']));
         }
-        
+
         if(!empty($postcode)) {
             if(strlen($region) == 2) {
                 $content->push(' ', new aura\html\Element('span', $postcode, ['class' => 'postal-code']));
@@ -112,27 +122,27 @@ class Address extends Base implements core\IDumpable {
                 if(!$isFull && !$content->isEmpty()) {
                     $content->push(', ');
                 }
-                
+
                 $content->push(new aura\html\Element($blockTag, $postcode, ['class' => 'postal-code']));
             }
         }
-        
-        if(!empty($countryCode)) {
+
+        if($this->_shouldShowCountry && !empty($countryCode)) {
             if(!$isFull && !$content->isEmpty()) {
                 $content->push(', ');
             }
-            
+
             if($isShort) {
                 $country = $countryCode;
             } else {
                 $country = $view->i18n->countries->getName($countryCode);
             }
-            
+
             $content->push(new aura\html\Element(
                 $blockTag, $country, ['class' => 'country-name']
             ));
         }
-        
+
         return $tag->renderWith($content);
     }
 
@@ -143,20 +153,20 @@ class Address extends Base implements core\IDumpable {
             case self::FULL:
                 $this->_mode = $mode;
                 break;
-           
+
             default:
                 $this->_mode = self::FULL;
                 break;
         }
-        
+
         return $this;
     }
-    
+
     public function getMode() {
         return $this->_mode;
     }
-    
-    
+
+
 // Dump
     public function getDumpProperties() {
         return [
