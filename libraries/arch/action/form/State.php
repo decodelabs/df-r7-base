f8abf9c42aa36a378bf71af266cc6265f1f29f53
Tom Wright <tom@inflatablecookie.com>
2015-11-20 16:11:19 +0000

    Added backup state handler
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/arch/action/form/State.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\arch\action\form;
+
+use df;
+use df\core;
+use df\arch;
+
+class State extends arch\node\form\State {
+
+}
