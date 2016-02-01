9f325b85adf05199453dbfcdc72659c266dbf469
Tom Wright <tom@inflatablecookie.com>
2016-02-01 14:02:05 +0000

    Use which php in script launcher if no binary path set in config
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/halo/process/Base.php
+++ libraries/halo/process/Base.php
@@ -10,19 +10,19 @@ use df\core;
 use df\halo;
 
 abstract class Base implements IProcess {
-    
+
     private static $_current;
-    
+
     protected $_processId;
     protected $_title;
-    
+
     public static function getCurrent() {
         if(!self::$_current) {
             $class = self::_getSystemClass();
             $pid = $class::getCurrentProcessId();
             self::$_current = new $class($pid, 'Current process');
         }
-        
+
         return self::$_current;
     }
 
@@ -34,10 +34,10 @@ abstract class Base implements IProcess {
     protected static function _getSystemClass() {
         $system = halo\system\Base::getInstance();
         $class = 'df\\halo\\process\\'.$system->getOSName().'Managed';
-        
+
         if(!class_exists($class)) {
             $class = 'df\\halo\\process\\'.$system->getPlatformType().'Managed';
-            
+
             if(!class_exists($class)) {
                 throw new halo\process\RuntimeException(
                     'Sorry, managed processes aren\'t currently supported on this platform!'
@@ -47,8 +47,8 @@ abstract class Base implements IProcess {
 
         return $class;
     }
-    
-    
+
+
     public static function launch($process, $args=null, $path=null, core\io\IMultiplexer $multiplexer=null, $user=null) {
         return self::newLauncher($process, $args, $path)
             ->setMultiplexer($multiplexer)
@@ -75,7 +75,7 @@ abstract class Base implements IProcess {
             ->setUser($user)
             ->launchBackground();
     }
-    
+
     public static function launchManaged($process, $args=null, $path=null, $user=null) {
         return self::newLauncher($process, $args, $path)
             ->setUser($user)
@@ -95,6 +95,11 @@ abstract class Base implements IProcess {
     public static function newScriptLauncher($path, $args=null) {
         $envConfig = core\Environment::getInstance();
         $binaryPath = $envConfig->getBinaryPath('php');
+
+        if($binaryPath === 'php') {
+            $binaryPath = halo\system\Base::getInstance()->which('php');
+        }
+
         $phpName = basename($binaryPath);
         $phpPath = null;
 
@@ -104,18 +109,18 @@ abstract class Base implements IProcess {
 
         return halo\process\launcher\Base::factory($phpName, [trim($path), $args], $phpPath);
     }
-    
-    
-    
+
+
+
     public function __construct($processId, $title) {
         $this->_processId = $processId;
         $this->_title = $title;
     }
-    
+
     public function getProcessId() {
         return $this->_processId;
     }
-    
+
     public function getTitle() {
         return $this->_title;
     }
