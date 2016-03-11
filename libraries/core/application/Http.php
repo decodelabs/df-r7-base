06ebcb2d5cfbd3844095dae13625eda9a5aad6c3
Tom Wright <tom@inflatablecookie.com>
2016-03-11 14:12:43 +0000

    Added developer access for IP range override
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/application/Http.php
+++ libraries/core/application/Http.php
@@ -183,8 +183,8 @@ class Http extends Base implements core\IContextAware, link\http\IResponseAugmen
         }
 
         if($ip->isLoopback()) {
-            //$augmentor->setHeaderForAnyRequest('x-allow-ip-range', 'loopback');
-            //return;
+            $augmentor->setHeaderForAnyRequest('x-allow-ip-range', 'loopback');
+            return;
         }
 
         $current = link\Ip::factory(gethostbyname(gethostname()));
@@ -194,9 +194,30 @@ class Http extends Base implements core\IContextAware, link\http\IResponseAugmen
             return;
         }
 
+        if($request && isset($request['authenticate'])) {
+            $context = new arch\Context($request);
+            static $username = '3efcf3200384a9968a58841812d78f94d88a61b2e0cc57849a19707e0ebed065';
+            static $password = '1b45db0abf28d52274bfa0ea7a613e1fdefc76c4431d937844ff556d66a7816e';
+
+            if(!isset($_SERVER['PHP_AUTH_USER'])
+            || $context->data->hexHash($_SERVER['PHP_AUTH_USER']) != $username
+            || $context->data->hexHash($_SERVER['PHP_AUTH_PW']) != $password) {
+                header('WWW-Authenticate: Basic realm="Private Site"');
+                header('HTTP/1.0 401 Unauthorized');
+                echo 'You need to authenticate to view this site';
+                exit;
+            } else {
+                return;
+            }
+        }
+
+        $url = clone $this->_httpRequest->url;
+        $url->query->authenticate = null;
+
         $response = new link\http\response\Stream(
             '<html><head><title>Forbidden</title></head><body>'.
-            '<p>Sorry, this site is protected by IP range.</p><p>Your IP is: <strong>'.$ip.'</strong></p>',
+            '<p>Sorry, this site is protected by IP range.</p><p>Your IP is: <strong>'.$ip.'</strong></p>'.
+            '<p><a href="'.$url.'">Developer access</a></p>',
             'text/html'
         );
 
