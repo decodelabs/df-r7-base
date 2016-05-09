3e41f9a8d94ddeff4744b0dc76d6860bfbf4c9b3
Tom Wright <tom@inflatablecookie.com>
2016-05-09 19:28:38 +0100

    Test dump
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/arch/mail/Base.php
+++ libraries/arch/mail/Base.php
@@ -28,6 +28,7 @@ abstract class Base extends aura\view\Mail implements IMail {
         $class = 'df\\apex\\directory\\'.$location->getArea().'\\'.implode('\\', $parts);
 
         if(!class_exists($class)) {
+            core\dump($class);
             throw new RuntimeException('Mail '.$origPath.' could not be found');
         }
 
