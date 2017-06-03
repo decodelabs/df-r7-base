22a7cd1006eecc8a86395f17030aafa6750e07f1
Tom Wright <tom@inflatablecookie.com>
2017-06-03 14:28:53 +0100

    Added type awareness to restApi nodes
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/arch/node/RestApi.php
+++ libraries/arch/node/RestApi.php
@@ -32,13 +32,17 @@ abstract class RestApi extends Base implements IRestApiNode {
         $this->authorizeRequest();
 
         $httpMethod = $this->_httpRequest->getMethod();
-        $func = 'execute'.ucfirst(strtolower($httpMethod));
+        $func = 'execute'.ucfirst(strtolower($httpMethod)).'As'.$this->request->getType();
 
         if(!method_exists($this, $func)) {
-            throw core\Error::EApi([
-                'message' => 'Node does not support '.$httpMethod.' method',
-                'http' => 400
-            ]);
+            $func = 'execute'.ucfirst(strtolower($httpMethod));
+
+            if(!method_exists($this, $func)) {
+                throw core\Error::EApi([
+                    'message' => 'Node does not support '.$httpMethod.' method',
+                    'http' => 400
+                ]);
+            }
         }
 
         $response = $this->{$func}();
