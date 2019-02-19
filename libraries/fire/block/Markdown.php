67adefe8583d257f75c0ad81c65c1262149411f2
Tom Wright <tom@inflatablecookie.com>
2019-02-19 18:07:40 +0000

    Fixed markdown editor
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/fire/block/Markdown.php
+++ libraries/fire/block/Markdown.php
@@ -12,50 +12,58 @@ use df\arch;
 use df\flex;
 use df\aura;
 
-class Markdown extends Base {
-
+class Markdown extends Base
+{
     const DEFAULT_CATEGORIES = ['Description'];
 
     protected $_body;
 
-    public function getFormat(): string {
+    public function getFormat(): string
+    {
         return 'markup';
     }
 
-    public function setBody($body) {
+    public function setBody($body)
+    {
         $this->_body = trim($body);
         return $this;
     }
 
-    public function getBody() {
+    public function getBody()
+    {
         return $this->_body;
     }
 
 
-    public function isEmpty(): bool {
+    public function isEmpty(): bool
+    {
         return !strlen(trim($this->_body));
     }
 
-    public function getTransitionValue() {
+    public function getTransitionValue()
+    {
         return $this->_body;
     }
 
-    public function setTransitionValue($value) {
+    public function setTransitionValue($value)
+    {
         $this->_body = $value;
         return $this;
     }
 
 
 
-// Io
-    public function readXml(flex\xml\IReadable $reader) {
+    // Io
+    public function readXml(flex\xml\IReadable $reader)
+    {
         $this->_validateXmlReader($reader);
 
         $this->_body = $reader->getFirstCDataSection();
         return $this;
     }
 
-    public function writeXml(flex\xml\IWritable $writer) {
+    public function writeXml(flex\xml\IWritable $writer)
+    {
         $this->_startWriterBlockElement($writer);
         $writer->writeCData($this->_body);
         $this->_endWriterBlockElement($writer);
@@ -64,8 +72,9 @@ class Markdown extends Base {
     }
 
 
-// Render
-    public function render() {
+    // Render
+    public function render()
+    {
         $view = $this->getView();
 
         return $view->html('div.block', $view->html->markdown($this->_body))
@@ -73,34 +82,35 @@ class Markdown extends Base {
     }
 
 
-// Form
-    public function loadFormDelegate(arch\IContext $context, arch\node\IFormState $state, arch\node\IFormEventDescriptor $event, string $id): arch\node\IDelegate {
+    // Form
+    public function loadFormDelegate(arch\IContext $context, arch\node\IFormState $state, arch\node\IFormEventDescriptor $event, string $id): arch\node\IDelegate
+    {
         return new class($this, ...func_get_args()) extends Base_Delegate {
-
-            protected function setDefaultValues() {
+            protected function setDefaultValues()
+            {
                 $this->values->body = $this->_block->getBody();
             }
 
-            public function renderFieldContent(aura\html\widget\Field $field) {
+            public function renderFieldContent(aura\html\widget\Field $field)
+            {
                 $this->view
                     ->linkCss('asset://lib/simplemde/simplemde.min.css', 100)
-                    ->linkJs('asset://lib/simplemde/simplemde.min.js', 100);
+                    //->linkJs('asset://lib/simplemde/simplemde.min.js', 100)
+                    ->dfKit->load('df-kit/markdown')
+                    ;
 
                 $field->push(
                     $ta = $this->html->textarea($this->fieldName('body'), $this->values->body)
                         //->isRequired($this->_isRequired)
-                        ->setId($id = uniqid('markdown'))
                         ->addClass('editor markdown')
-                );
-
-                $this->view->addFootScript($id,
-                    'var '.$id.' = new SimpleMDE({ element: $("#'.$id.'")[0] });'
+                        ->setDataAttribute('editor', 'markdown')
                 );
 
                 return $this;
             }
 
-            public function apply() {
+            public function apply()
+            {
                 $validator = $this->data->newValidator()
                     ->addField('body', 'text')
                         ->isRequired($this->_isRequired)
