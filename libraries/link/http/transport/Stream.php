f3a8f6b64f5d3ebdcf3e9c94bd48a468d782d820
Tom Wright <tom@inflatablecookie.com>
2019-04-05 17:59:34 +0100

    Updated verify-peer SSL options in HTTP client
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/link/http/transport/Stream.php
+++ libraries/link/http/transport/Stream.php
@@ -9,30 +9,31 @@ use df;
 use df\core;
 use df\link;
 
-class Stream implements link\http\ITransport {
-    
+class Stream implements link\http\ITransport
+{
     private $_headerStack = null;
     private $_headers;
 
-    public function promiseResponse(link\http\IRequest $request, link\http\IClient $client) {
-        return core\lang\Promise::defer(function($input, $promise) use($request, $client) {
+    public function promiseResponse(link\http\IRequest $request, link\http\IClient $client)
+    {
+        return core\lang\Promise::defer(function ($input, $promise) use ($request, $client) {
             $maxRedirects = null;
 
-            while(true) {
+            while (true) {
                 $response = $this->_sendRequest($request, $client, $promise);
 
-                if(!$response->isRedirect()) {
+                if (!$response->isRedirect()) {
                     break;
                 }
 
-                if($maxRedirects === null) {
+                if ($maxRedirects === null) {
                     $maxRedirects = $request->options->maxRedirects;
                 }
 
-                if(!$maxRedirects) {
+                if (!$maxRedirects) {
                     break;
                 }
-                
+
                 $request = clone $request;
                 $request->setUrl($url = $response->headers->get('location'));
 
@@ -44,12 +45,15 @@ class Stream implements link\http\ITransport {
         });
     }
 
-    protected function _sendRequest(link\http\IRequest $request, link\http\IClient $client, core\lang\IPromise $promise) {
+    protected function _sendRequest(link\http\IRequest $request, link\http\IClient $client, core\lang\IPromise $promise)
+    {
         $this->_prepareRequest($request, $client);
         $stream = $this->_createStream($request, $promise);
 
         $response = new link\http\response\Stream(
-            null, null, $this->_headers
+            null,
+            null,
+            $this->_headers
         );
 
         $client->prepareResponse($response, $request);
@@ -61,27 +65,29 @@ class Stream implements link\http\ITransport {
         return $response;
     }
 
-    protected function _prepareRequest(link\http\IRequest $request, link\http\IClient $client) {
+    protected function _prepareRequest(link\http\IRequest $request, link\http\IClient $client)
+    {
         $client->prepareRequest($request);
         $request->headers->remove('expect');
 
-        if($request->headers->getHttpVersion() == '1.1'
+        if ($request->headers->getHttpVersion() == '1.1'
         && !$request->headers->has('connection')) {
             $request->headers->set('connection', 'close');
         }
     }
 
-    protected function _createStream(link\http\IRequest $request, core\lang\IPromise $promise) {
+    protected function _createStream(link\http\IRequest $request, core\lang\IPromise $promise)
+    {
         $this->_headerStack = null;
         $context = $this->_createContextOptions($request);
 
         $context = stream_context_create($context, [
-            'notification' => function($id, $severity, $message, $code, $current, $total) use($promise, $request, &$http_response_header) {
-                if($this->_headerStack === null) {
+            'notification' => function ($id, $severity, $message, $code, $current, $total) use ($promise, $request, &$http_response_header) {
+                if ($this->_headerStack === null) {
                     $this->_headerStack = &$http_response_header;
                 }
 
-                switch($id) {
+                switch ($id) {
                     case STREAM_NOTIFY_CONNECT:
                         $promise->emit('connect', ['request' => $request]);
                         break;
@@ -99,7 +105,7 @@ class Stream implements link\http\ITransport {
                         break;
 
                     case STREAM_NOTIFY_REDIRECTED:
-                        if($this->_headerStack) {
+                        if ($this->_headerStack) {
                             $this->_headers = link\http\response\HeaderCollection::fromResponseArray($this->_headerStack);
                             $this->_headerStack = [];
                         } else {
@@ -124,14 +130,15 @@ class Stream implements link\http\ITransport {
         $pointer = fopen($request->url, core\fs\Mode::READ_ONLY, null, $context);
         $this->_headers = link\http\response\HeaderCollection::fromResponseArray($this->_headerStack);
 
-        if($this->_headers->get('transfer-encoding') == 'chunked') {
+        if ($this->_headers->get('transfer-encoding') == 'chunked') {
             stream_filter_append($pointer, 'Streams_ChunkFilter', STREAM_FILTER_READ);
         }
 
         return new core\io\Stream($pointer);
     }
 
-    protected function _createContextOptions(link\http\IRequest $request) {
+    protected function _createContextOptions(link\http\IRequest $request)
+    {
         $output = [
             'http' => [
                 'method' => strtoupper($request->getMethod()),
@@ -143,36 +150,37 @@ class Stream implements link\http\ITransport {
             ],
             'ssl' => [
                 'verify_peer' => $request->options->verifySsl,
+                'verify_peer_name' => $request->options->verifySsl,
                 'allow_self_signed' => $request->options->allowSelfSigned
             ]
         ];
 
-        if($request->options->timeout) {
+        if ($request->options->timeout) {
             $output['http']['timeout'] = $request->options->timeout;
         }
 
         $body = $request->getBodyDataString();
 
-        if(strlen($body)) {
+        if (strlen($body)) {
             $output['http']['content'] = $body;
 
-            if(!$request->headers->has('content-type')) {
+            if (!$request->headers->has('content-type')) {
                 $output['http']['header'] .= 'Content-Type:';
             }
         }
-        
-        if($request->options->verifySsl) {
-            if($request->options->caBundlePath) {
+
+        if ($request->options->verifySsl) {
+            if ($request->options->caBundlePath) {
                 $output['ssl']['cafile'] = $request->options->caBundlePath;
-            } else if(PHP_VERSION_ID < 50600) {
+            } elseif (PHP_VERSION_ID < 50600) {
                 $output['ssl']['cafile'] = link\http\Client::getDefaultCaBundlePath();
             }
         }
 
-        if($request->options->certPath) {
+        if ($request->options->certPath) {
             $output['ssl']['local_cert'] = $request->options->certPath;
 
-            if($request->options->certPassword !== null) {
+            if ($request->options->certPassword !== null) {
                 $output['ssl']['passphrase'] = $request->options->certPassword;
             }
         }
@@ -184,30 +192,31 @@ class Stream implements link\http\ITransport {
 }
 
 
-class Streams_ChunkFilter extends \php_user_filter {
-    
+class Streams_ChunkFilter extends \php_user_filter
+{
     protected $_remaining = 0;
 
-    function filter($in, $out, &$consumed, $closing) {
-        while($bucket = stream_bucket_make_writeable($in)) {
+    public function filter($in, $out, &$consumed, $closing)
+    {
+        while ($bucket = stream_bucket_make_writeable($in)) {
             $outbuffer = '';
             $offset = 0;
 
-            while($offset < $bucket->datalen) {
-                if($this->_remaining === 0) {
+            while ($offset < $bucket->datalen) {
+                if ($this->_remaining === 0) {
                     $firstLine = strpos($bucket->data, "\r\n", $offset);
                     $descriptor = substr($bucket->data, $offset, $firstLine - $offset);
                     $length = current(explode(';', $descriptor, 2));
                     $length = trim($length);
 
-                    if(!ctype_xdigit($length)) {
+                    if (!ctype_xdigit($length)) {
                         return \PSFS_ERR_FATAL;
                     }
 
                     $this->_remaining = hexdec($length);
                     $offset = $firstLine + 2;
 
-                    if($this->_remaining === 0) {
+                    if ($this->_remaining === 0) {
                         break;
                     }
                 }
@@ -216,7 +225,7 @@ class Streams_ChunkFilter extends \php_user_filter {
                 $size = strlen($nibble);
                 $offset += $size;
 
-                if($size === $this->_remaining) {
+                if ($size === $this->_remaining) {
                     $offset += 2;
                 }
 
