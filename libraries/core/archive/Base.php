a18f85b5b7215b529080f3b20aabd64dad381b44
Tom Wright <tom@inflatablecookie.com>
2019-05-15 15:40:08 +0100

    Fixed decompress path handler
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/archive/Base.php
+++ libraries/core/archive/Base.php
@@ -8,18 +8,19 @@ namespace df\core\archive;
 use df;
 use df\core;
 
-abstract class Base implements IArchive {
-
-    public static function extract($file, $destination=null, $flattenRoot=false) {
-        if(preg_match('/\.zip$/i', $file)) {
+abstract class Base implements IArchive
+{
+    public static function extract($file, $destination=null, $flattenRoot=false)
+    {
+        if (preg_match('/\.zip$/i', $file)) {
             $type = 'Zip';
-        } else if(preg_match('/\.tar(\.(gz|bz2))?$/i', $file)) {
+        } elseif (preg_match('/\.tar(\.(gz|bz2))?$/i', $file)) {
             $type = 'Tar';
-        } else if(preg_match('/\.gz$/i', $file)) {
+        } elseif (preg_match('/\.gz$/i', $file)) {
             $type = 'Gz';
-        } else if(preg_match('/\.rar$/i', $file)) {
+        } elseif (preg_match('/\.rar$/i', $file)) {
             $type = 'Rar';
-        } else if(preg_match('/\.bz2$/i', $file)) {
+        } elseif (preg_match('/\.bz2$/i', $file)) {
             $type = 'Bz2';
         } else {
             throw core\Error::ERuntime('Unable to detect type of archive: '.$file);
@@ -28,48 +29,57 @@ abstract class Base implements IArchive {
         return self::factory($type)->extractFile($file, $destination, $flattenRoot);
     }
 
-    public static function factory(string $type): IArchive {
+    public static function factory(string $type): IArchive
+    {
         $class = 'df\\core\\archive\\'.ucfirst($type);
 
-        if(!class_exists($class)) {
+        if (!class_exists($class)) {
             throw core\Error::EUnsupported('Archive type '.$type.' is not supported');
         }
 
         return new $class();
     }
 
-    public function __construct() {}
+    public function __construct()
+    {
+    }
 
-    public function getType(): string {
+    public function getType(): string
+    {
         return (new \ReflectionObject($this))->getShortName($this);
     }
 
-    public function extractFile(string $file, string $destDir=null, bool $flattenRoot=false): string {
+    public function extractFile(string $file, string $destDir=null, bool $flattenRoot=false): string
+    {
         throw core\Error::EUnsupported($this->getType().' type archives cannot handle file and folder compression');
     }
 
-    public function decompressFile(string $file, string $destFile=null): string {
+    public function decompressFile(string $file, string $destFile=null): string
+    {
         throw core\Error::EUnsupported($this->getType().' type archives cannot handle file and folder compression');
     }
 
-    public function compressString(string $string): string {
+    public function compressString(string $string): string
+    {
         throw core\Error::EUnsupported($this->getType().' type archives cannot handle string compression');
     }
 
-    public function decompressString(string $string): string {
+    public function decompressString(string $string): string
+    {
         throw core\Error::EUnsupported($this->getType().' type archives cannot handle string compression');
     }
 
-    protected function _normalizeExtractDestination(string &$file, string $destination): string {
+    protected function _normalizeExtractDestination(string &$file, string $destination): string
+    {
         $file = str_replace(['/', '\\'], \DIRECTORY_SEPARATOR, realpath($file));
 
-        if(!is_file($file)) {
+        if (!is_file($file)) {
             throw core\Error::ENotFound(
                 'Source archive could not be found: '.$file
             );
         }
 
-        if($destination === null) {
+        if ($destination === null) {
             $destination = dirname($file);
         }
 
@@ -77,24 +87,25 @@ abstract class Base implements IArchive {
         return $destination;
     }
 
-    protected function _normalizeDecompressDestination(string &$file, string $destFile, string $extension): string {
+    protected function _normalizeDecompressDestination(string &$file, ?string $destFile, string $extension): string
+    {
         $file = str_replace(['/', '\\'], \DIRECTORY_SEPARATOR, realpath($file));
 
-        if($destFile !== null) {
+        if ($destFile !== null) {
             $destFile = str_replace('\\', '/', $destFile);
 
-            if(false === strpos($destFile, '/')) {
+            if (false === strpos($destFile, '/')) {
                 $destFile = dirname($file).'/'.$destFile;
             }
         }
 
-        if(!is_file($file)) {
+        if (!is_file($file)) {
             throw core\Error::ENotFound(
                 'Source archive could not be found: '.$file
             );
         }
 
-        if($destFile === null) {
+        if ($destFile === null) {
             $destFile = dirname($file).'/'.$this->_getDecompressFileName($file, $extension);
         }
 
@@ -102,11 +113,12 @@ abstract class Base implements IArchive {
         return $destFile;
     }
 
-    protected function _getDecompressFileName(string $file, string $extension): string {
+    protected function _getDecompressFileName(string $file, string $extension): string
+    {
         $fileName = basename($file);
         $extLength = 1 + strlen($extension);
 
-        if(strtolower(substr($fileName, -$extLength)) == '.'.$extension) {
+        if (strtolower(substr($fileName, -$extLength)) == '.'.$extension) {
             $fileName = substr($fileName, 0, -$extLength);
         } else {
             $fileName .= '-extract';
@@ -115,16 +127,17 @@ abstract class Base implements IArchive {
         return $fileName;
     }
 
-    protected function _flattenRoot(string $destination): void {
+    protected function _flattenRoot(string $destination): void
+    {
         $dir = core\fs\Dir::factory($destination);
         $dirName = null;
 
-        foreach($dir->scan() as $item) {
-            if($item instanceof core\fs\IFile) {
+        foreach ($dir->scan() as $item) {
+            if ($item instanceof core\fs\IFile) {
                 return;
             }
 
-            if($dirName !== null) {
+            if ($dirName !== null) {
                 return;
             }
 
