b6d3b91d7be81501a4469a353333dadb186eb629
Tom Wright <tom@inflatablecookie.com>
2019-06-12 13:25:31 +0100

    Refactored maxmind class
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/link/geoIp/adapter/MaxMindDb.php
+++ libraries/link/geoIp/adapter/MaxMindDb.php
@@ -10,22 +10,23 @@ use df\core;
 use df\link;
 use df\opal;
 
-class MaxMindDb implements link\geoIp\IAdapter {
-
+class MaxMindDb implements link\geoIp\IAdapter
+{
     use link\geoIp\TAdapter;
 
     protected $_reader;
 
-    public static function fromConfig() {
+    public static function fromConfig()
+    {
         $file = self::_getFileFromConfig();
 
-        if(empty($file)) {
+        if (empty($file)) {
             throw new link\geoIp\RuntimeException(
                 'MaxMind DB file has not been set in config'
             );
         }
 
-        if(!is_file($file)) {
+        if (!is_file($file)) {
             throw new link\geoIp\RuntimeException(
                 'MaxMind DB file could not be found'
             );
@@ -34,84 +35,88 @@ class MaxMindDb implements link\geoIp\IAdapter {
         return new self(new opal\mmdb\Reader($file));
     }
 
-    public static function isAvailable() {
+    public static function isAvailable()
+    {
         $file = self::_getFileFromConfig();
         return !empty($file) && is_file($file);
     }
 
-    protected static function _getFileFromConfig() {
+    protected static function _getFileFromConfig()
+    {
         $config = link\geoIp\Config::getInstance();
         $settings = $config->getSettingsFor('MaxMindDb');
         $file = $settings['file'];
 
-        if(dirname($file) == '.') {
+        if (dirname($file) == '.') {
             $file = df\Launchpad::$app->getLocalDataPath().'/geoIp/'.$file;
         }
 
         return $file;
     }
 
-    public function __construct(opal\mmdb\IReader $reader) {
+    public function __construct(opal\mmdb\IReader $reader)
+    {
         $this->_reader = $reader;
     }
 
-    public function lookup(link\IIp $ip, link\geoIp\IResult $result) {
+    public function lookup(link\IIp $ip, link\geoIp\IResult $result)
+    {
         $data = $this->_reader->get($ip);
 
-        if($data === null) {
+        if ($data === null) {
             return $result;
         }
 
-        if(isset($data['continent']['code'])) {
+        if (isset($data['continent']['code'])) {
             $result->continent = $data['continent']['code'];
             $result->continentName = $data['continent']['names']['en'];
 
-            if(isset($data['continent']['geoname_id'])) {
+            if (isset($data['continent']['geoname_id'])) {
                 $result->continentGeonameId = $data['continent']['geoname_id'];
             }
         }
 
-        if(isset($data['country']['iso_code'])) {
+        if (isset($data['country']['iso_code'])) {
             $result->country = $data['country']['iso_code'];
             $result->countryName = $data['country']['names']['en'];
 
-            if(isset($data['country']['geoname_id'])) {
+            if (isset($data['country']['geoname_id'])) {
                 $result->countryGeonameId = $data['country']['geoname_id'];
             }
         }
 
-        if(isset($data['subdivisions'])) {
-            if($region = array_pop($data['subdivisions'])) {
+        if (isset($data['subdivisions'])) {
+            if ($region = array_pop($data['subdivisions'])) {
                 $result->regionName = $region['names']['en'];
 
-                if(isset($region['iso_code'])) {
+                if (isset($region['iso_code'])) {
                     $result->region = $region['iso_code'];
                 }
 
-                if(isset($region['geoname_id'])) {
+                if (isset($region['geoname_id'])) {
                     $result->regionGeonameId = $region['geoname_id'];
                 }
             }
         }
 
-        if(isset($data['city']['names'])) {
+        if (isset($data['city']['names'])) {
             $result->cityName = $data['city']['names']['en'];
 
-            if(isset($data['city']['geoname_id'])) {
+            if (isset($data['city']['geoname_id'])) {
                 $result->cityGeonameId = $data['city']['geoname_id'];
             }
         }
 
-        if(isset($data['postal']['code'])) {
+        if (isset($data['postal']['code'])) {
             $result->postcode = $data['postal']['code'];
         }
 
-        if(isset($data['location']['latitude'])) {
+        if (isset($data['location']['latitude'])) {
             $result->latitude = $data['location']['latitude'];
             $result->longitude = $data['location']['longitude'];
         }
 
-        if(isset($data['location']['time_zone'])) {
+        if (isset($data['location']['time_zone'])) {
             $result->timezone = $data['location']['time_zone'];
         }
 
