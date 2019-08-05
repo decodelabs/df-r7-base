37611832e051e3616139a388bef444e18722ac65
Tom Wright <tom@inflatablecookie.com>
2019-08-05 21:28:17 +0100

    Updated delete form fields
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/arch/scaffold/node/DeleteForm.php
+++ libraries/arch/scaffold/node/DeleteForm.php
@@ -10,55 +10,61 @@ use df\core;
 use df\arch;
 use df\aura;
 
-class DeleteForm extends arch\node\DeleteForm {
-
+class DeleteForm extends arch\node\DeleteForm
+{
     protected $_scaffold;
 
-    public function __construct(arch\scaffold\IScaffold $scaffold) {
+    public function __construct(arch\scaffold\IScaffold $scaffold)
+    {
         $this->_scaffold = $scaffold;
         parent::__construct($scaffold->getContext());
     }
 
-    protected function getInstanceId() {
+    protected function getInstanceId()
+    {
         return $this->_scaffold->getRecordId();
     }
 
-    protected function getItemName() {
+    protected function getItemName()
+    {
         return $this->_scaffold->getRecordItemName();
     }
 
-    protected function createItemUi($container) {
+    protected function createItemUi($container)
+    {
         $container->push(
             $this->apex->component(ucfirst($this->_scaffold->getRecordKeyName()).'Details')
                 ->setRecord($this->_scaffold->getRecord())
         );
 
-        foreach($this->_scaffold->getRecordDeleteFlags() as $key => $label) {
-            $container->push(
+        foreach ($this->_scaffold->getRecordDeleteFlags() as $key => $label) {
+            $container->addField()->addClass('stacked')->push(
                 $this->html->checkbox($key, $this->values->{$key}, $label)
             );
         }
     }
 
-    protected function apply() {
+    protected function apply()
+    {
         $flags = $this->_scaffold->getRecordDeleteFlags();
         $validator = $this->data->newValidator();
 
-        foreach($flags as $key => $label) {
+        foreach ($flags as $key => $label) {
             $validator->addField($key, 'boolean');
         }
 
         $validator->validate($this->values);
 
-        foreach($flags as $key => $label) {
+        foreach ($flags as $key => $label) {
             $flags[$key] = $validator[$key];
         }
 
         $this->_scaffold->deleteRecord($this->_scaffold->getRecord(), $flags);
     }
 
-    protected function finalize() {
-        return $this->complete(function() {
+    protected function finalize()
+    {
+        return $this->complete(function () {
             //return $this->uri->directoryRequest($this->_scaffold->getRecordBackLinkRequest());
         });
     }
