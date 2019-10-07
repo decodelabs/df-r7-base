5b6952fd1dbe2a25db8434b50f066617d93b0f09
Tom Wright <tom@inflatablecookie.com>
2019-10-07 11:28:50 +0100

    Updated daemon/restart-all output
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/daemons/_nodes/TaskRestartAll.php
+++ directory/front/daemons/_nodes/TaskRestartAll.php
@@ -11,45 +11,38 @@ use df\apex;
 use df\arch;
 use df\halo;
 
-class TaskRestartAll extends arch\node\Task {
-
+class TaskRestartAll extends arch\node\Task
+{
     use TDaemonTask;
 
-    public function execute() {
-        if(!$hasRestarted = $this->_hasRestarted()) {
-            $this->io->write('Looking up daemon list...');
-        }
-
+    public function execute()
+    {
         $daemons = halo\daemon\Base::loadAll();
 
-        foreach($daemons as $name => $daemon) {
-            if(!$daemon::AUTOMATIC || $daemon::TEST_MODE) {
+        foreach ($daemons as $name => $daemon) {
+            if (!$daemon::AUTOMATIC || $daemon::TEST_MODE) {
                 unset($daemons[$name]);
                 continue;
             }
 
             $remote = halo\daemon\Remote::factory($daemon);
 
-            if($remote->isRunning()) {
+            if ($remote->isRunning()) {
                 $this->_ensurePrivileges();
             }
         }
 
-        if(!$hasRestarted) {
-            $this->io->writeLine(' found '.count($daemons).' to restart');
-        }
-
-        if(empty($daemons)) {
+        if (empty($daemons)) {
             return;
         }
 
         $this->task->shouldCaptureBackgroundTasks(true);
         $this->io->indent();
 
-        foreach($daemons as $name => $daemon) {
+        foreach ($daemons as $name => $daemon) {
             $remote = halo\daemon\Remote::factory($daemon);
 
-            if($remote->isRunning()) {
+            if ($remote->isRunning()) {
                 $remote->setMultiplexer($this->io);
                 $remote->restart();
             } else {
