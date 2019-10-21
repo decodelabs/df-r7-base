4d1d61c6df5cccc74ded2f53757c515cee2279a2
Tom Wright <tom@inflatablecookie.com>
2019-10-21 18:31:10 +0100

    Updated php stan config
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- PHPStanBootstrap.php
+++ PHPStanBootstrap.php
@@ -8,3 +8,8 @@ use DecodeLabs\Glitch;
 $startTime = df\Launchpad::initEnvironment();
 df\Launchpad::initLoaders(__DIR__, $startTime);
 Glitch::setRunMode('development');
+
+df\Launchpad::$loader->loadPackages([
+    'nightfire', 'postal', 'spearmint',
+    'touchstone', 'webCore'
+]);
