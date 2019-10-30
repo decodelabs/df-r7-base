331322351de4fbe7143ba1bbec473aa0608bc9e3
Tom Wright <tom@inflatablecookie.com>
2019-10-30 00:01:20 +0000

    Updated daemons
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/halo/daemon/Remote.php
+++ libraries/halo/daemon/Remote.php
@@ -185,7 +185,7 @@ class Remote implements IRemote
             ->thenIf($this->_session, function ($launcher) {
                 $launcher->setIoBroker($this->_session->getBroker());
             })
-            ->setDecoratable(false)
+            //->setDecoratable(false)
             ->launch();
     }
 }
