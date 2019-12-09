3159ba8bdda2cf590f5826728f23042ef4d9a561
Tom Wright <tom@inflatablecookie.com>
2019-12-09 16:32:59 +0000

    Added 4byte check to text validator
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/core/validate/field/Text.php
+++ libraries/core/validate/field/Text.php
@@ -10,8 +10,8 @@ use df\core;
 use df\opal;
 use df\flex;
 
-class Text extends Base implements core\validate\ITextField {
-
+class Text extends Base implements core\validate\ITextField
+{
     use core\validate\TStorageAwareField;
     use core\validate\TRecordManipulatorField;
     use opal\query\TFilterConsumer;
@@ -23,11 +23,13 @@ class Text extends Base implements core\validate\ITextField {
     protected $_minWords = null;
     protected $_maxWords = null;
     protected $_shouldTrim = true;
+    protected $_accept4Byte = false;
 
 
-// Pattern
-    public function setPattern($pattern) {
-        if(empty($pattern)) {
+    // Pattern
+    public function setPattern($pattern)
+    {
+        if (empty($pattern)) {
             $pattern = null;
         }
 
@@ -35,21 +37,23 @@ class Text extends Base implements core\validate\ITextField {
         return $this;
     }
 
-    public function getPattern() {
+    public function getPattern()
+    {
         return $this->_pattern;
     }
 
 
-// Word length
-    public function setMinWords($length) {
-        if($length !== null) {
+    // Word length
+    public function setMinWords($length)
+    {
+        if ($length !== null) {
             $length = (int)$length;
 
-            if(empty($length)) {
+            if (empty($length)) {
                 $length = 0;
             }
 
-            if($length < 0) {
+            if ($length < 0) {
                 $length = 0;
             }
         }
@@ -58,19 +62,21 @@ class Text extends Base implements core\validate\ITextField {
         return $this;
     }
 
-    public function getMinWords() {
+    public function getMinWords()
+    {
         return $this->_minWords;
     }
 
-    public function setMaxWords($length) {
-        if($length !== null) {
+    public function setMaxWords($length)
+    {
+        if ($length !== null) {
             $length = (int)$length;
 
-            if(empty($length)) {
+            if (empty($length)) {
                 $length = 0;
             }
 
-            if($length < 0) {
+            if ($length < 0) {
                 $length = 0;
             }
         }
@@ -79,14 +85,16 @@ class Text extends Base implements core\validate\ITextField {
         return $this;
     }
 
-    public function getMaxWords() {
+    public function getMaxWords()
+    {
         return $this->_maxWords;
     }
 
 
-// Trim
-    public function shouldTrim(bool $flag=null) {
-        if($flag !== null) {
+    // Trim
+    public function shouldTrim(bool $flag=null)
+    {
+        if ($flag !== null) {
             $this->_shouldTrim = $flag;
             return $this;
         }
@@ -95,32 +103,58 @@ class Text extends Base implements core\validate\ITextField {
     }
 
 
-// Validate
-    public function validate() {
+    // 4 Byte
+    public function canAccept4Byte(bool $flag=null)
+    {
+        if ($flag != null) {
+            $this->_accept4Byte = $flag;
+            return $this;
+        }
+
+        return $this->_accept4Byte;
+    }
+
+
+    // Validate
+    public function validate()
+    {
         // Sanitize
         $value = $this->data->getValue();
 
-        if($this->_shouldTrim) {
+        if ($this->_shouldTrim) {
             $value = trim($value);
         }
 
         $value = $this->_sanitizeValue($value);
 
 
-        if(!$length = $this->_checkRequired($value)) {
+        if (!$length = $this->_checkRequired($value)) {
             return null;
         }
 
 
 
         // Validate
+        if (
+            !$this->_accept4Byte &&
+            preg_match('%(?:
+                  \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+                | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+                | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+            )%xs', $value)
+        ) {
+            $this->addError('4byte', $this->validator->_('Sorry, we can\'t currently accept emojis'));
+            return;
+        }
+
+
         $this->_validateMinLength($value, $length);
         $this->_validateMaxLength($value, $length);
 
-        if($this->_minWords !== null || $this->_maxWords !== null) {
+        if ($this->_minWords !== null || $this->_maxWords !== null) {
             $wordCount = flex\Text::countWords($value);
 
-            if($this->_minWords !== null && $wordCount < $this->_minWords) {
+            if ($this->_minWords !== null && $wordCount < $this->_minWords) {
                 $this->addError('minWords', $this->validator->_(
                     [
                         'n = 1' => 'This field must contain at least %min% word',
@@ -131,7 +165,7 @@ class Text extends Base implements core\validate\ITextField {
                 ));
             }
 
-            if($this->_maxWords !== null && $wordCount > $this->_maxWords) {
+            if ($this->_maxWords !== null && $wordCount > $this->_maxWords) {
                 $this->addError('maxWords', $this->validator->_(
                     [
                         'n = 1' => 'This field must not be more than %max% word',
@@ -143,7 +177,7 @@ class Text extends Base implements core\validate\ITextField {
             }
         }
 
-        if($this->_pattern !== null && !filter_var(
+        if ($this->_pattern !== null && !filter_var(
             $value, FILTER_VALIDATE_REGEXP,
             ['options' => ['regexp' => $this->_pattern]]
         )) {
