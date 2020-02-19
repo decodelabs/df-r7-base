47f56f8f693cf6ac59f156b16605d7078505dbdf
Tom Wright <tom@inflatablecookie.com>
2020-02-19 15:15:29 +0000

    Updated test domain for local avatars

--- libraries/plug/Avatar.php
+++ libraries/plug/Avatar.php
@@ -11,37 +11,40 @@ use df\plug;
 use df\arch;
 use df\link;
 
-class Avatar implements arch\IDirectoryHelper {
-
+class Avatar implements arch\IDirectoryHelper
+{
     use arch\TDirectoryHelper;
 
     const GRAVATAR_BASE = '//www.gravatar.com/avatar/';
 
-    public function getClientAvatarUrl($size=null) {
+    public function getClientAvatarUrl($size=null)
+    {
         return $this->getAvatarUrl($this->context->user->client->getId(), $size);
     }
 
-    public function getAvatarUrl($userId, $size=null) {
+    public function getAvatarUrl($userId, $size=null)
+    {
         return $this->context->uri->__invoke(
             'avatar/download?user='.$userId.'&size='.$size.'&c='.$this->context->data->user->cache->getAvatarCacheTime()
         );
     }
 
-    public function getGravatarUrl($email, $size=null, $default='mm') {
+    public function getGravatarUrl($email, $size=null, $default='mm')
+    {
         $hash = md5(trim(strtolower($email)));
         $output = new link\http\Url(self::GRAVATAR_BASE.$hash);
 
-        if($size !== null) {
+        if ($size !== null) {
             $output->query->s = (int)$size;
         }
 
-        if($url = $this->_getDefaultAvatarImageUrl()) {
-            if(substr($url->getDomain(), -4) == '.dev' ||
-                substr($url->getDomain(), -6) == '.test' || 
+        if ($url = $this->_getDefaultAvatarImageUrl()) {
+            if (substr($url->getDomain(), -4) == '.dev' ||
+                substr($url->getDomain(), -13) == '.localtest.me' ||
                 $url->hasCredentials()) {
                 $this->_defaultImageUrl = null;
             } else {
-                if($size !== null) {
+                if ($size !== null) {
                     $url = clone $url;
                     $url->path->setFilename('default-'.$size);
                 }
@@ -50,7 +53,7 @@ class Avatar implements arch\IDirectoryHelper {
             }
         }
 
-        if($default !== null) {
+        if ($default !== null) {
             $output->query->d = $default;
         }
 
@@ -59,18 +62,19 @@ class Avatar implements arch\IDirectoryHelper {
 
     private $_defaultImageUrl = false;
 
-    protected function _getDefaultAvatarImageUrl() {
-        if($this->_defaultImageUrl === false) {
+    protected function _getDefaultAvatarImageUrl()
+    {
+        if ($this->_defaultImageUrl === false) {
             $path = $this->context->data->user->avatarConfig->getDefaultAvatarPath();
             $this->_defaultImageUrl = null;
 
-            if($path) {
+            if ($path) {
                 $path = new core\uri\Path($path);
                 $this->_defaultImageUrl = $this->context->uri->__invoke('avatar/download?user=default&type='.$path->getExtension());
 
                 $config = core\app\runner\http\Config::getInstance();
 
-                if($credentials = $config->getCredentials()) {
+                if ($credentials = $config->getCredentials()) {
                     $this->_defaultImageUrl->setCredentials(
                         $credentials['username'],
                         $credentials['password']
