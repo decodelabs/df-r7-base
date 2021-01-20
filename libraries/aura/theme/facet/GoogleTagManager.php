6b4e3ab3de08106c51a4ea0e2cb70198c3c1fe2f
Tom Wright <tom@inflatablecookie.com>
2021-01-20 16:40:55 +0000

    Ported Google Tag Manager facet from Acowtancy
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ libraries/aura/theme/facet/GoogleTagManager.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+namespace df\aura\theme\facet;
+
+use df;
+use df\core;
+use df\aura;
+use df\arch;
+use df\spur;
+
+class GoogleTagManager extends Base
+{
+    protected $_id;
+    protected $_devAuth = null;
+    protected $_devEnv = null;
+
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+        $this->_id = $config['id'] ?? null;
+        $this->_devAuth = $config['devAuth'] ?? null;
+        $this->_devEnv = $config['devEnv'] ?? null;
+    }
+
+    public function setId(string $id)
+    {
+        $this->_id = $id;
+        return $this;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->_id;
+    }
+
+
+
+    public function setDevAuth(?string $auth)
+    {
+        $this->_devAuth = $auth;
+        return $this;
+    }
+
+    public function getDevAuth(): ?string
+    {
+        return $this->_devAuth;
+    }
+
+    public function setDevEnv(?string $env)
+    {
+        $this->_devEnv = $env;
+        return $this;
+    }
+
+    public function getDevEnv(): ?string
+    {
+        return $this->_devEnv;
+    }
+
+
+
+    public function onHtmlViewLayoutRender(aura\view\IHtmlView $view, $content)
+    {
+        if (
+            !$this->_checkEnvironment() ||
+            !$view->consent->has('statistics', 'marketing')
+        ) {
+            return;
+        }
+
+        $devSuffix = $devQuery = '';
+
+        if (
+            !$view->app->isProduction() &&
+            !empty($this->_devAuth) &&
+            !empty($this->_devEnv)
+        ) {
+            $devQuery = '&gtm_auth='.$this->_devAuth.'&gtm_preview='.$this->_devEnv.'&gtm_cookies_win=x';
+            $devSuffix = "+ '".$devQuery."'";
+        }
+
+        $view->addScript('gtm',
+            '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':'."\n".
+            'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],'."\n".
+            'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src='."\n".
+            '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl'.$devSuffix.';f.parentNode.insertBefore(j,f);'."\n".
+            '})(window,document,\'script\',\'dataLayer\',\''.$this->_id.'\');'
+        );
+
+        $content =
+            '<!-- Google Tag Manager (noscript) -->'."\n".
+            '<noscript><iframe src="//www.googletagmanager.com/ns.html?id='.$this->_id.$devQuery.'"'."\n".
+            'height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'."\n".
+            '<!-- End Google Tag Manager (noscript) -->'."\n".
+            $content
+            ;
+
+        return $content;
+    }
+}
