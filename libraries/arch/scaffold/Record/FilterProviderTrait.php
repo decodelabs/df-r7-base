5c3ff64a8c33bb82e03eee883a18e864b008069f
Tom Wright <tom@inflatablecookie.com>
2021-03-08 14:15:21 +0000

    Removed keyName hidden in switchers
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/arch/scaffold/Record/FilterProviderTrait.php
+++ libraries/arch/scaffold/Record/FilterProviderTrait.php
@@ -80,9 +80,6 @@ trait FilterProviderTrait
 
         $form = $this->html->form(null, 'get');
         $form->addFieldSet()->addClass('scaffold switcher')->push(
-            isset($this->request[$keyName]) && !isset($index[$keyName]) ?
-                $this->html->hidden($keyName, $this->getRecordId()) : null,
-
             isset($this->request['od']) ?
                 $this->html->hidden('od', $this->request['od']) : null,
 
