f9027542ced3173f0cf7fc8fee269886a06d18b9
Tom Wright <tom@inflatablecookie.com>
2021-04-16 17:28:13 +0000

    Updated testing bindings
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -26,6 +26,7 @@ if ($hasAppFile) {
 
 if (class_exists($appClass)) {
     df\Launchpad::$loader->loadPackages(array_keys($appClass::PACKAGES));
+    $appClass::setupVeneerBindings();
 }
 
 error_reporting(false);
