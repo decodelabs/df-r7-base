c1d51ea87c6e1e9a560d0958b3c1b45b6d47b5e0
Tom Wright <tom@inflatablecookie.com>
2021-05-11 13:21:39 +0000

    Added veneer stub task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ directory/front/_nodes/TaskVeneerStub.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of the Decode Framework
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace df\apex\directory\front\_nodes;
+
+use df\arch;
+
+use DecodeLabs\Atlas;
+use DecodeLabs\Terminus as Cli;
+use DecodeLabs\Veneer;
+use DecodeLabs\Veneer\Stub\Generator;
+
+class TaskVeneerStub extends arch\node\Task
+{
+
+    public function execute()
+    {
+        // Get bindings
+        $manager = Veneer::getDefaultManager();
+        $bindings = $manager->getBindings();
+
+        // Prepare arguments
+        Cli::getCommandDefinition()
+            ->addArgument('task', 'Task entry')
+            ->addArgument('?binding', 'Name of the binding');
+        Cli::prepareArguments();
+
+        // Ask for binding name
+        if (null === ($bindingName = Cli::getArgument('binding'))) {
+            $bindingName = Cli::newQuestion('Which binding do you want to stub?')
+                ->setOptions(...array_keys($bindings))
+                ->prompt();
+        }
+
+        $bindingName = ucfirst($bindingName);
+
+        // Load binding
+        if (!isset($bindings[$bindingName])) {
+            Cli::operative('Binding not found');
+            exit;
+        }
+
+        $binding = $bindings[$bindingName];
+
+
+        // Generate
+        $dir = Atlas::createDir($this->app->getPath() . '/stubs');
+        $generator = new Generator($dir);
+        $generator->generate($binding);
+
+        Cli::success('done');
+    }
+}
