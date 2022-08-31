2ae9e264f4c71ad78d33b0d7a4471a0d6c8e459e
Tom Wright <tom@inflatablecookie.com>
2022-08-31 11:00:40 +0000

    Added nonce support to GTM
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/aura/theme/facet/GoogleTagManager.php
+++ libraries/aura/theme/facet/GoogleTagManager.php
@@ -92,7 +92,8 @@ class GoogleTagManager extends Base
             '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':'."\n".
             'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],'."\n".
             'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src='."\n".
-            '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl'.$devSuffix.';f.parentNode.insertBefore(j,f);'."\n".
+            '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl'.$devSuffix.';'."\n".
+            'var n=d.querySelector(‘[nonce]’); n&&j.setAttribute(‘nonce’,n.nonce||n.getAttribute(‘nonce’));f.parentNode.insertBefore(j,f);'."\n".
             '})(window,document,\'script\',\'dataLayer\',\''.$this->_id.'\');'
         );
 
