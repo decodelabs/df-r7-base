35a2312bdd3ffe53e4485135b175f040a0b0b26b
Tom Wright <tom@inflatablecookie.com>
2022-08-31 14:36:21 +0000

    Updated GTM nonce handling
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/aura/theme/facet/GoogleTagManager.php
+++ libraries/aura/theme/facet/GoogleTagManager.php
@@ -93,7 +93,9 @@ class GoogleTagManager extends Base
             'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],'."\n".
             'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src='."\n".
             '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl'.$devSuffix.';'."\n".
-            'var n=d.querySelector(\'[nonce]\'); n&&j.setAttribute(\'nonce\',n.nonce||n.getAttribute(\'nonce\'));f.parentNode.insertBefore(j,f);'."\n".
+            'var h=\'\';var n=d.querySelector(\'[nonce]\'); n&&j.setAttribute(\'nonce\',h=(n.nonce||n.getAttribute(\'nonce\')));'."\n".
+            'w[l] = w[l] || []; w[l].push({ nonce: h });'."\n".
+            'f.parentNode.insertBefore(j,f);'."\n".
             '})(window,document,\'script\',\'dataLayer\',\''.$this->_id.'\');'
         );
 
