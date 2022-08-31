228cd0ca977b4abdc1f16e59eef29669567884ae
Tom Wright <tom@inflatablecookie.com>
2022-08-31 11:03:21 +0000

    Fixed GTM plugin
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/aura/theme/facet/GoogleTagManager.php
+++ libraries/aura/theme/facet/GoogleTagManager.php
@@ -93,7 +93,7 @@ class GoogleTagManager extends Base
             'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],'."\n".
             'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src='."\n".
             '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl'.$devSuffix.';'."\n".
-            'var n=d.querySelector(‘[nonce]’); n&&j.setAttribute(‘nonce’,n.nonce||n.getAttribute(‘nonce’));f.parentNode.insertBefore(j,f);'."\n".
+            'var n=d.querySelector(\'[nonce]\'); n&&j.setAttribute(\'nonce\',n.nonce||n.getAttribute(\'nonce\'));f.parentNode.insertBefore(j,f);'."\n".
             '})(window,document,\'script\',\'dataLayer\',\''.$this->_id.'\');'
         );
 
