2b9fe804b2f436abf89674041975176de121797a
Tom Wright <tom@inflatablecookie.com>
2022-10-03 21:24:05 +0000

    Updated daemon restart task
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/daemons/_nodes/TaskRestartAll.php
+++ directory/front/daemons/_nodes/TaskRestartAll.php
@@ -3,6 +3,7 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
+
 namespace df\apex\directory\front\daemons\_nodes;
 
 use df;
@@ -30,7 +31,7 @@ class TaskRestartAll extends arch\node\Task
             $remote = halo\daemon\Remote::factory($daemon);
 
             if ($remote->isRunning()) {
-                $this->_ensurePrivileges();
+                //$this->_ensurePrivileges();
             }
         }
 
