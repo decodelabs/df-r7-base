b5ec5855f48103e9594fd54c25035ceacdf0eff4
Tom Wright <tom@inflatablecookie.com>
2023-10-18 10:37:02 +0000

    Updated Genesis
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- Df.php
+++ Df.php
@@ -11,7 +11,7 @@ if (array_pop($parts) != 'entry') {
 $appPath = implode('/', $parts);
 
 // Load Genesis bootstrap
-require_once $appPath.'/vendor/decodelabs/genesis/src/bootstrap.php';
+require_once $appPath.'/vendor/decodelabs/genesis/src/Bootstrap.php';
 require_once __DIR__.'/provider/Genesis/Bootstrap.php';
 
 (new DecodeLabs\R7\Genesis\Bootstrap(__DIR__, $appPath))->run();
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'vendor/autoload.php';
-require_once 'vendor/decodelabs/genesis/src/bootstrap.php';
+require_once 'vendor/decodelabs/genesis/src/Bootstrap.php';
 
 use DecodeLabs\Genesis;
 use DecodeLabs\R7\Genesis\Hub;
