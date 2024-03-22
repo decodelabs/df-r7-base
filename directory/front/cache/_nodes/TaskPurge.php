1ecc60d2d5ffa4a7ed4c50c290863d2e253ff953
Tom Wright <tom@inflatablecookie.com>
2024-03-22 09:48:43 +0000

    Clear legacy file store on build
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/front/cache/_nodes/TaskPurge.php
+++ directory/front/cache/_nodes/TaskPurge.php
@@ -8,6 +8,8 @@ namespace df\apex\directory\front\cache\_nodes;
 
 use DecodeLabs\R7\Config\Cache as CacheConfig;
 use DecodeLabs\R7\Legacy;
+use DecodeLabs\Atlas;
+use DecodeLabs\Genesis;
 use DecodeLabs\Stash;
 use DecodeLabs\Terminus as Cli;
 
@@ -31,5 +33,20 @@ class TaskPurge extends arch\node\Task
         }
 
         Stash::purge();
+
+        // Delete
+        $paths = [
+            Genesis::$hub->getLocalDataPath().'/filestore',
+            Genesis::$hub->getLocalDataPath().'/fileStore',
+        ];
+
+        foreach($paths as $path) {
+            $dir = Atlas::dir($path);
+
+            if($dir->exists()) {
+                Cli::{'.green'}('Legacy file store');
+                $dir->delete();
+            }
+        }
     }
 }
